72265ee4913e3a591eef33fdb4f929f42aaa9c12
Will Robertson <wspr81@gmail.com>
2009-10-27 15:51:24 +1030

    A template for writing literate matlab code

---
+++ matlab/magnetforces.m
@@ -0,0 +1,27 @@
+ 
+  [forces torques]  =  function magnetforces(magnet_fixed, magnet_floating); 
+ 
+   
+ 
+  a1  =  0.5 * magnet_fixed.dim(1); 
+  b1  =  0.5 * magnet_fixed.dim(2); 
+  c1  =  0.5 * magnet_fixed.dim(3); 
+  a2  =  0.5 * magnet_floating.dim(1); 
+  b2  =  0.5 * magnet_floating.dim(2); 
+  c2  =  0.5 * magnet_floating.dim(3); 
+ 
+ 
+   
+ 
+ 
+   
+ 
+ 
+   
+ 
+ 
+    
+ 
+  return; 
+ 
+
