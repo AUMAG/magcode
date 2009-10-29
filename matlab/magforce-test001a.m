ccbce362ce26d1acb6010355cb95392c27c4eabc
Will Robertson <wspr81@gmail.com>
2009-10-29 17:53:41 +1030

    add a test file (no results yet!)

---
+++ matlab/magforce-test001a.m
@@ -0,0 +1,11 @@
+ 
+ 
+magnet_fixed.dim  =  [0.02 0.04 0.06]; 
+magnet_fixed.magn  =  1.3; 
+magnet_fixed.madir  =  [0 0]; % vertical 
+ 
+magnet_float.dim  =  [0.07 0.05 0.03]; 
+magnet_float.magn  =  1.1; 
+magnet_float.madir  =  [0 0]; % vertical 
+ 
+magnet_disp  =  [0.1 0.15 0.05]; 
