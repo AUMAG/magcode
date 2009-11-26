65686abb588d2e420c828789f45668f6fe1032dd
Will Robertson <wspr81@gmail.com>
2009-11-26 12:43:04 +1030

    start to generalise a little and fix some names

--- matlab/multipoleforces.m
+++ matlab/multipoleforces.m
@@ -37,41 +37,50 @@ end
  
  
   
-M  =  prod(fixed_array.mcount); 
-N  =  prod(float_array.mcount); 
+if ~isfield(fixed_array,'type') 
+  fixed_array.type  =  'generic'; 
+end 
  
-fixed_magnet_loc  =  repmat(NaN,[M 3]); 
-float_magnet_loc  =  repmat(NaN,[N 3]); 
+switch fixed_array.type 
  
-fixed_magnet_magdir  =  repmat(NaN,[M 2]); 
-float_magnet_magdir  =  repmat(NaN,[N 2]); 
+end 
+ 
+fixed_array.total  =  prod(fixed_array.mcount); 
+fixed_array.loc  =  repmat(NaN,[fixed_array.total 3]); 
+fixed_array.magdir  =  repmat(NaN,[fixed_array.total 2]); 
+ 
+float_array.total  =  prod(float_array.mcount); 
+float_array.loc  =  repmat(NaN,[float_array.total 3]); 
+float_array.magdir  =  repmat(NaN,[float_array.total 2]); 
+ 
  
   
 if length(fixed_array.msize) == 3 
-  fixed_magnet_dim_array  =   ... 
+  fixed_array.dim_array  =   ... 
       repmat(reshape(fixed_array.msize,[1 1 1 3]), fixed_array.mcount); 
-  fixed_magnet_dim  =  reshape(fixed_magnet_dim_array, [M 3]); 
+  fixed_array.dim  =  reshape(fixed_array.dim_array, [fixed_array.total 3]); 
 else 
   error('Not yet implemented.') 
 end 
  
 if length(float_array.msize) == 3 
-  float_magnet_dim_array  =   ... 
+  float_array.dim_array  =   ... 
       repmat(reshape(float_array.msize,[1 1 1 3]), float_array.mcount); 
-  float_magnet_dim  =  reshape(float_magnet_dim_array, [N 3]); 
+  float_array.dim  =  reshape(float_array.dim_array, [float_array.total 3]); 
 else 
   error('Not yet implemented.') 
 end 
  
+ 
   
 if length(fixed_array.magn) == 1 
-  fixed_magnet_magn  =  repmat(fixed_array.magn,[M 1]); 
+  fixed_array.magn  =  repmat(fixed_array.magn,[fixed_array.total 1]); 
 else 
   error('Not yet implemented.') 
 end 
  
 if length(float_array.magn) == 1 
-  float_magnet_magn  =  repmat(float_array.magn,[N 1]); 
+  float_array.magn  =  repmat(float_array.magn,[float_array.total 1]); 
 else 
   error('Not yet implemented.') 
 end 
@@ -93,28 +102,6 @@ else
   error('Not yet implemented.') 
 end 
  
-  
-ii  =  0; 
-for xx  =  1:fixed_array.mcount(1) 
-  for yy  =  1:fixed_array.mcount(2) 
-    for zz  =  1:fixed_array.mcount(3) 
-      ii  =  ii + 1; 
-      fixed_magnet_loc(ii,:)  =   ... 
-        [xx-1; yy-1; zz-1].*(squeeze(fixed_magnet_dim_array(xx,yy,zz,:))+fixed_gaps); 
-    end 
-  end 
-end 
- 
-ii  =  0; 
-for xx  =  1:float_array.mcount(1) 
-  for yy  =  1:float_array.mcount(2) 
-    for zz  =  1:float_array.mcount(3) 
-      ii  =  ii + 1; 
-      float_magnet_loc(ii,:)  =   ... 
-        [xx-1; yy-1; zz-1].*(squeeze(float_magnet_dim_array(xx,yy,zz,:))+float_gaps); 
-    end 
-  end 
-end 
  
   
 ii  =  0; 
@@ -122,51 +109,85 @@ for xx  =  1:fixed_array.mcount(1)
   for yy  =  1:fixed_array.mcount(2) 
     for zz  =  1:fixed_array.mcount(3) 
       ii  =  ii + 1; 
-      fixed_magnet_magdir(ii,:)  =  fixed_array.magdir_fn(xx,yy,zz); 
+      fixed_array.magdir(ii,:)  =  fixed_array.magdir_fn(xx,yy,zz); 
     end 
   end 
 end 
  
+debug_disp('Fixed magnetisation directions') 
+debug_disp(mod(fixed_array.magdir,360)) 
  
 ii  =  0; 
 for xx  =  1:float_array.mcount(1) 
   for yy  =  1:float_array.mcount(2) 
     for zz  =  1:float_array.mcount(3) 
       ii  =  ii + 1; 
-      float_magnet_magdir(ii,:)  =  float_array.magdir_fn(xx,yy,zz); 
+      float_array.magdir(ii,:)  =  float_array.magdir_fn(xx,yy,zz); 
     end 
   end 
 end 
  
+debug_disp('Float magnetisation directions') 
+debug_disp(mod(float_array.magdir,360)) 
+ 
+ 
+  
+ii  =  0; 
+for xx  =  1:fixed_array.mcount(1) 
+  for yy  =  1:fixed_array.mcount(2) 
+    for zz  =  1:fixed_array.mcount(3) 
+      ii  =  ii + 1; 
+      fixed_array.loc(ii,:)  =   ... 
+        [xx-1; yy-1; zz-1].*(squeeze(fixed_array.dim_array(xx,yy,zz,:))+fixed_gaps); 
+    end 
+  end 
+end 
+ 
+debug_disp('Fixed magnet locations:') 
+debug_disp(fixed_array.loc) 
+ 
+ii  =  0; 
+for xx  =  1:float_array.mcount(1) 
+  for yy  =  1:float_array.mcount(2) 
+    for zz  =  1:float_array.mcount(3) 
+      ii  =  ii + 1; 
+      float_array.loc(ii,:)  =   ... 
+        [xx-1; yy-1; zz-1].*(squeeze(float_array.dim_array(xx,yy,zz,:))+float_gaps); 
+    end 
+  end 
+end 
+ 
+debug_disp('Float magnet locations:') 
+debug_disp(float_array.loc) 
  
  
  
   
 if calc_force_bool 
-  array_forces  =  repmat(NaN,[M N 3]); 
+  array_forces  =  repmat(NaN,[fixed_array.total float_array.total 3]); 
 end 
  
 if calc_stiffness_bool 
-  array_stiffnesses  =  repmat(NaN,[M N 3]); 
+  array_stiffnesses  =  repmat(NaN,[fixed_array.total float_array.total 3]); 
 end 
  
-for mm  =  1:M 
+for mm  =  1:fixed_array.total 
  
   fixed_magnet  =  struct( ... 
- 'dim',    fixed_magnet_dim(mm,:),  ... 
- 'magn',   fixed_magnet_magn(mm),  ... 
- 'magdir', fixed_magnet_magdir(mm,:)  ... 
+ 'dim',    fixed_array.dim(mm,:),  ... 
+ 'magn',   fixed_array.magn(mm),  ... 
+ 'magdir', fixed_array.magdir(mm,:)  ... 
   ); 
  
-  for nn  =  1:N 
+  for nn  =  1:float_array.total 
  
     float_magnet  =  struct( ... 
-      'dim',    float_magnet_dim(nn,:),  ... 
-      'magn',   float_magnet_magn(nn),  ... 
-      'magdir', float_magnet_magdir(nn,:)  ... 
+      'dim',    float_array.dim(nn,:),  ... 
+      'magn',   float_array.magn(nn),  ... 
+      'magdir', float_array.magdir(nn,:)  ... 
     ); 
  
-    displ  =  displ - fixed_magnet_loc(mm,:) + float_magnet_loc(nn,:); 
+    displ  =  displ - fixed_array.loc(mm,:) + float_array.loc(nn,:); 
  
     if calc_force_bool 
       array_forces(mm,nn,:)  =   ... 
@@ -207,5 +228,11 @@ end
  
  
  
+ 
+function debug_disp(str) 
+  %disp(str) 
+end 
+ 
+end 
  
 
