c9c5c4e3c0d91ead8d435d00ece9b9a4d4a6ad47
Will Robertson <wspr81@gmail.com>
2009-12-09 16:29:16 +1030

    save calculated data in the comparison
    some of these can take a while

--- examples/multipole_compare.m
+++ examples/multipole_compare.m
@@ -9,55 +9,67 @@ clc
 timestamp
 tic
 
-%% Common setup
+%% Calculate
 
-displ_steps = 5;
-zrange = linspace(0.0101,0.03,displ_steps);
+if exist('multipole_compare_data.mat')
 
-Nmag_per_wave_array = [1 2 4 8];
-Nwaves_array = [1 2 4];
-height = 0.01;
-
-forces = repmat(NaN,...
-  [length(Nwaves_array) length(Nmag_per_wave_array) displ_steps 3]);
-
-%% Calculate 
-
-for nn = 1:length(Nwaves_array)
+  load multipole_compare_data.mat
   
-  Nwaves = Nwaves_array(nn);
+else
+  
+  displ_steps = 50;
+  zrange = linspace(0.0101,0.02,displ_steps);
+  
+  Nmag_per_wave_array = [1 2 4 8];
+  Nwaves_array = [1 2 4];
+  
+  array_height = 0.01;
+  array_length = 0.1;
+  
+  forces = repmat(NaN,...
+    [length(Nwaves_array) length(Nmag_per_wave_array) displ_steps 3]);
+  
+  
+  for nn = 1:length(Nwaves_array)
     
-  for ww = 1:length(Nmag_per_wave_array)
+    Nwaves = Nwaves_array(nn);
     
-    Nmag_per_wave = Nmag_per_wave_array(ww);
-    
-    fixed_array = ...
-      struct(...
-      'type','linear-x',  ...
-      'face','up',        ...
-      'length', 0.10,     ...
-      'width',  0.01,     ...
-      'height', height,   ...
-      'Nmag_per_wave', Nmag_per_wave, ...
-      'Nwaves', Nwaves,   ...
-      'magn', 1,          ...
-      'magdir_first', 90  ...
-      );
-    
-    float_array = fixed_array;
-    float_array.face = 'down';
-    float_array.magdir_first = -90;
-        
-    for ii = 1:N
-      displ = [0 0 zrange(ii)];
-      forces(nn,ww,ii,:) = multipoleforces(fixed_array, float_array, displ);
+    for ww = 1:length(Nmag_per_wave_array)
+      
+      Nmag_per_wave = Nmag_per_wave_array(ww);
+      
+      fixed_array = ...
+        struct(...
+        'type','linear-x',  ...
+        'face','up',        ...
+        'length', array_length,   ...
+        'width',  array_height,   ...
+        'height', array_height,   ...
+        'Nmag_per_wave', Nmag_per_wave, ...
+        'Nwaves', Nwaves,   ...
+        'magn', 1,          ...
+        'magdir_first', 90  ...
+        );
+      
+      float_array = fixed_array;
+      float_array.face = 'down';
+      float_array.magdir_first = -90;
+      
+      for ii = 1:displ_steps
+        displ = [0 0 zrange(ii)];
+        forces(nn,ww,ii,:) = multipoleforces(fixed_array, float_array, displ);
+      end
+      
     end
-        
-  end
     
-end
+  end
+  
+  toc
+  
+  save multipole_compare_data displ_steps zrange ...
+    Nmag_per_wave_array Nwaves_array array_height array_length forces
 
-toc
+end
 
 %% Plot
 
@@ -72,13 +84,12 @@ for nn = 1:length(Nwaves_array)
     
     Nmag_per_wave = Nmag_per_wave_array(ww);
         
-    plot(zrange/height,squeeze(forces(nn,ww,:,3)),'Tag',num2str(Nmag_per_wave));
+    plot(zrange/array_height,squeeze(forces(nn,ww,:,3)),'Tag',num2str(Nmag_per_wave));
     
   end
   
-  set(gca,'xlim',[0.009 zrange(end)]);
-  h1 = draworigin([0.01 0],'v');
-  set(h1,'linestyle','--');
+  set(gca,'xlim',[0.009 zrange(end)]/array_height);
+  h1 = draworigin([1 0],'v'); set(h1,'linestyle','--');
   colourplot
   labelplot('northeast','vertical','$M$')
   legendshrink
@@ -98,13 +109,12 @@ for ww = 1:length(Nmag_per_wave_array)
   for nn = 1:length(Nwaves_array)
     
     Nwaves = Nwaves_array(nn);    
-    plot(zrange,squeeze(forces(nn,ww,:,3)),'Tag',num2str(Nwaves));
+    plot(zrange/array_height,squeeze(forces(nn,ww,:,3)),'Tag',num2str(Nwaves));
     
   end
   
-  set(gca,'xlim',[0.009 zrange(end)]);
-  h1 = draworigin([0.01 0],'v');
-  set(h1,'linestyle','--');
+  set(gca,'xlim',[0.009 zrange(end)]/array_height);
+  h1 = draworigin([1 0],'v'); set(h1,'linestyle','--');
   colourplot
   labelplot('northeast','vertical','$N$')
   legendshrink
