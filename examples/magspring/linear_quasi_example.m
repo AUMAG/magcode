f3ef6d19414d764b0c9c9f7302c5ab9f495a907d
Will Robertson <wspr81@gmail.com>
2010-02-11 17:16:18 +1030

    Another example (will they ever stop?)

---
+++ examples/magspring/linear_quasi_example.m
@@ -0,0 +1,129 @@
+%% Linear quasi-Halbach arrays with varying sizes of magnet
+%
+%
+% In which the vertically oriented magnets have different sizes to the
+% horizontally oriented magnets.
+
+
+%% Calculate
+
+ratios_array = [0 0.5 1];
+
+datafile = 'data/linear_quasi_example.mat';
+if exist(datafile,'file')
+  
+  current_array = ratios_array;
+  load(datafile);
+  if isequal(current_array,ratios_array)
+    recalculate = false;
+  else
+    ratios_array = current_array;
+    recalculate = true;
+  end
+end
+
+if recalculate
+  
+  displ_steps = 50;
+  zrange = linspace(0.0101,0.02,displ_steps);
+  displ = [0; 0; 1]*zrange;
+  
+  array_height = 0.01;
+  array_length = 0.1;
+  
+  forces = repmat(NaN,...
+    [3 displ_steps length(ratios_array)]);
+  
+  
+  for nn = 1:length(ratios_array)
+    
+    ratio = ratios_array(nn);
+    
+    fixed_array = ...
+      struct(...
+      'type','linear-quasi',    ...
+      'align','x',        ...
+      'face','up',        ...
+      'length', array_length,   ...
+      'width',  array_height,   ...
+      'height', array_height,   ...
+      'Nwaves', 2 ,  ...
+      'ratio', ratio ...
+      );
+    
+    float_array = fixed_array;
+    float_array.face = 'down';
+    
+    forces(:,:,nn) = multipoleforces(fixed_array, float_array, displ);
+    
+  end
+  
+  save(datafile,'zrange','ratios_array','array_height','array_length','forces');
+
+end
+
+
+%% Plot
+
+style = {'.-','-','--'};
+
+zdata = zrange/array_height;
+
+figname = 'ratios-compare';
+willfig(figname,'small'); clf; hold on;
+  
+for ii = 1:length(some)
+      
+  plot(zdata,squeeze(forces(3,:,ii)),...
+      style{ii},...
+      'linewidth',1,...
+      'Tag',num2str( ratios_array(ii) ));
+
+end
+
+set(gca,'box','on','ticklength',[0.02 0.05])
+colourplot
+
+xlabel('Normalised vertical displacement')
+ylabel('Vertical force, N')
+
+H = labelplot('north','vertical','$\mupqratio$');
+pos = get(H,'position');
+set(H,'position',[0.6 0.5 pos(3:4)]);
+legendshrink(0.5)
+axistight(gca,0.1,'-x')
+set(gca,'ylim',[0 400]);
+draworigin([1 0],'v',':')
+
+matlabfrag(['fig/',figname],'dpi',3200);
+
+%% Plot a few normalised
+
+style = {'.-','-','--'};
+
+zdata = zrange/array_height;
+
+figname = 'ratios-compare-norm';
+willfig(figname); clf; hold on;
+
+for ii = 1:length(some)
+      
+  plot(zdata,squeeze(forces(3,:,ii))./squeeze(forces(3,:,3)),...
+      style{ii},...
+      'linewidth',1,...
+      'Tag',num2str( ratios_array(nn) ));
+
+end
+
+set(gca,'box','on','ticklength',[0.02 0.05])
+colourplot
+
+xlabel('Normalised vertical displacement')
+ylabel('Vertical force, N')
+
+H = labelplot('north','vertical','$\mupqratio$');
+pos = get(H,'position');
+set(H,'position',[0.6 0.5 pos(3:4)]);
+legendshrink(0.5)
+
+matlabfrag(['fig/',figname],'dpi',3200);
