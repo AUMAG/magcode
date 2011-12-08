632b87f95bea760045bc48faab974ab7d84f50b6
Will Robertson <wspr81@gmail.com>
2011-12-08 15:31:32 +1030

    add cylindrical example to "magnet ratios"

--- examples/magnet_ratios.m
+++ examples/magnet_ratios.m
@@ -16,7 +16,7 @@ else
   simple_graph = false;
 end
 
-%% Calculations
+%% Calculations for cuboid magnets
 
 
 m = 0.01;
@@ -36,24 +36,21 @@ ymin = 0.0001;
 yrange = linspace(ymin,ymax,Ny);
 
 % initialise variable
-forces = repmat(NaN,[3 Nm Ny]);
+forces = nan([3 Nm Ny]);
   
 for mm = 1:Nm
-  
   a = (volume*magratio(mm)^2)^(1/3);
   b = (volume/magratio(mm))^(1/3);
   
   for yy = 1:Ny
-    
-    face_gap = a+yrange(yy);
-    forces(:,mm,yy) = magnetforces(mag(a,b,1),mag(a,b,-1), [0;0; face_gap] );
-    
+    forces(:,mm,yy) = magnetforces(mag(a,b,1),mag(a,b,-1), [0;0; a+yrange(yy)] );
   end
 end
 
+
 %% Force versus displacement figures
 
-willfig('mag ratios1'); clf; hold on
+willfig('mag ratios1','small'); clf; hold on
 
 for mm = 1:4
   
@@ -78,7 +75,7 @@ if ~simple_graph
 end
 
 
-willfig('mag ratios2'); clf; hold on
+willfig('mag ratios2','small'); clf; hold on
 for mm = Nm-2:-1:4
   
   plot(1000*yrange,squeeze(forces(3,mm,:))./squeeze(forces(3,end,:)),'tag',num2str(magratio(mm)))
@@ -112,7 +109,7 @@ Nm = 100;
 magratio = linspace(0.1,1,Nm);
 
 % initialise variables
-forces = repmat(NaN,[3 Nm Ny]);
+forces = nan([3 Nm Ny]);
   
 for mm = 1:Nm
   a = (volume*magratio(mm)^2)^(1/3);
@@ -123,23 +120,24 @@ for mm = 1:Nm
 end
 
 
-willfig('mag ratios 3'); clf; hold on
+willfig('mag ratios 3','small'); clf; hold on
 
-for dd = 1:Ny
+for dd = 1:Ny %#ok<FORPF> :: plots should be in a certain order
   
   plot(magratio,squeeze(forces(3,:,dd)))
-  [yy ii] = max(squeeze(forces(3,:,dd)));
-  plot(magratio(ii),yy,'.');
+  [yy, ii] = max(squeeze(forces(3,:,dd)));
+  plot(magratio(ii),yy,'.','MarkerSize',8);
     
 end
 
 xlim([0.1 1])
-xlabel 'Magnet size ratio $\mbqmagratio$'
-ylabel 'Force, N'
+ylim([0 30])
+xlabel 'Magnet size ratio $\mbqmagratio$'    'interpreter' 'none'
+ylabel 'Force, N'                            'interpreter' 'none'
 
 set(gca,'xtick',0:0.2:1)
 
-H = annotation('textarrow',[0.77 0.77],[0.67 0.3]);
+H = annotation('textarrow',[0.77 0.77],[0.75 0.3]);
 set(H,...
   'HeadLength',6.5,'HeadWidth',4.5,'HeadStyle','cback3',...
   'String',{'Increasing','displacement'},'HorizontalAlignment','Center')
@@ -149,4 +147,143 @@ if ~simple_graph
   colourplot
   matlabfrag('fig/magratios')
 end
+
+
+
+%% And the same thing for cylindrical magnets
+%
+% Here the volume is fixed at
+%
+% $$ V = \pi r^2 a $$
+%
+% with magnet ratio
+%
+% $$ \alpha = r/a $$
+%
+% So, the radius and side length, resp., are
+%
+% $$ r = \left( \frac{\alpha V}{\pi} \right)^{1/3} $$
+%
+% $$ a = \left( \frac{V}{\pi \alpha^2} \right)^{1/3} $$
+
+m = 0.01;
+volume = m^3;
+
+cylmag = @(r,a,magdir) ...
+  struct('dim',[r a],'magn',1,'magdir',[0 0 magdir]);
+
+% magnet ratio range
+cylmagratio = [0.6:0.1:1.0, 1.2:0.2:2.0];
+Ncm = length(cylmagratio);
+Ncrit = find(cylmagratio==1.0);
+
+% displacement range
+ymax = 1.5*m;
+ymin = 0.0001;
+Ny = 50;
+yrange = linspace(ymin,ymax,Ny);
+
+% initialise variable
+cylforces = nan([3 Ncm Ny]);
+
+for mm = 1:Ncm
+  a = (volume/(pi*cylmagratio(mm)^2))^(1/3);
+  r = (volume*cylmagratio(mm)/pi)^(1/3);
+  
+  for yy = 1:Ny
+    cylforces(:,mm,yy) = magnetforces(cylmag(r,a,1),cylmag(r,a,-1), [0;0; a+yrange(yy)] );
+  end
+end
+
+willfig('cylmag ratios1');
+clf; hold on
+
+yy = 8;
+yyind = find(round(1000*yrange)==yy,1,'first');
+
+normforce = squeeze(cylforces(3,cylmagratio==1.0,:));
+
+for mm = 1:Ncrit-1
+  plot(1000*yrange,squeeze(cylforces(3,mm,:))./normforce,'tag',num2str(cylmagratio(mm)))
+  text(yy,squeeze(cylforces(3,mm,yyind))./normforce(yyind),num2str(cylmagratio(mm)) )
+end
+
+xlabel 'Displacement $x$, mm'
+ylabel 'Normalised force $\bar F$' 'interpreter' 'none'
+
+if ~simple_graph
+  axistight
+  colourplot
+  matlabfrag('fig/magratio1-cyl')
+end
+
+willfig('cylmag ratios2');
+clf; hold on
+
+yy = 1;
+yyind = find(round(1000*yrange)==yy,1,'first');
+
+normforce = squeeze(cylforces(3,cylmagratio==1.0,:));
+
+for mm = Ncrit+1:Ncm
+  plot(1000*yrange,squeeze(cylforces(3,mm,:))./normforce,'tag',num2str(cylmagratio(mm)))
+  text(yy,squeeze(cylforces(3,mm,yyind))./normforce(yyind),num2str(cylmagratio(mm)) )
+end
+
+xlabel 'Displacement $x$, mm'
+ylabel 'Normalised force $\bar F$' 'interpreter' 'none'
+
+if ~simple_graph
+  axistight
+  colourplot
+  matlabfrag('fig/magratio2-cyl')
+end
+
+
+%% Recalculate cylindrical case for force versus magnet ratio figure
+
+yrange = 0.001:0.001:m;
+Ny = length(yrange);
+
+% sizes
+Nm = 100;
+cylmagratio = linspace(0.1,2.5,Nm);
+
+% initialise variables
+cylforces = nan([3 Nm Ny]);
+ 
+for mm = 1:Nm
+  a = (volume/(pi*cylmagratio(mm)^2))^(1/3);
+  r = (volume*cylmagratio(mm)/pi)^(1/3);
+  for yy = 1:Ny
+    cylforces(:,mm,yy) = magnetforces(cylmag(r,a,1),cylmag(r,a,-1), [0;0; a+yrange(yy)] );
+  end
+end
+
+willfig('cylmag ratios 3','small'); clf; hold on
+
+for dd = 1:Ny %#ok<FORPF> :: plots should be in a certain order
+
+  plot(cylmagratio,squeeze(cylforces(3,:,dd)))
+  [yy, ii] = max(squeeze(cylforces(3,:,dd)));
+  plot(cylmagratio(ii),yy,'.','MarkerSize',8);
+
+end
+
+xlim([0 2.5])
+xlabel 'Magnet size ratio $\ratioMag$'       'interpreter' 'none'
+ylabel 'Force, N'                            'interpreter' 'none'
+
+H = annotation('textarrow',[0.77 0.77],[0.75 0.3]);
+set(H,...
+  'HeadLength',6.5,'HeadWidth',4.5,'HeadStyle','cback3',...
+  'String',{'Increasing','displacement'},'HorizontalAlignment','Center')
+
+
+if ~simple_graph
+  colourplot
+  matlabfrag('fig/magratios-cyl')
+end
+
+
 
