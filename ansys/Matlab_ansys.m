ea46e0ca2c06e3802e153efd063c8ca1da1d930b
unknown <jziggers@mecht-jz1.mecheng.adelaide.edu.au>
2012-08-09 15:32:04 +0930

    joost again!

--- ansys/Matlab_ansys.m
+++ ansys/Matlab_ansys.m
@@ -1,11 +1,15 @@
-clear all;close all;clc;
+%%This script compares analytical Matlab with numerical ANSYS results
 
+clear all;close all;clc;
+!del output.txt
+!del total.txt
+% Magnet 1 is upper magnet
 a=0.3;          %air gap width
 b=0.6;          %air gap height
 c=0.05;         %radius magnet 2
 e=0.14;         %distance between magnet 2 and bottom of airgap
 d=0.03;         %height of magnet 2
-x=0.4;         %distance between 2 magnets
+%x=0.03;        %distance between 2 magnets, not needed in this script
 w=0.03;         %height of magnet 1
 u=0.035;        %raduis magnet 1
 B1=1.2;         %magnetisation magnet 1 (T)
@@ -14,30 +18,35 @@ mu=1.05;        %permeability (N/A^2)
 mu0=4*pi*10^-7; %permeability of space (N/A^2)
 H1=B1/(mu*mu0); %coercivity magnet 1 (A/m)
 H2=B2/(mu*mu0); %coercivity magnet 2 (A/m)
-mesh=0.005;     %meshsize numerical solution
+mesh=0.005;     %meshsize numerical solver
 
 %% analytical solution
-%dimensions [radius height]
+% Dimensions [radius height]
 magnet_upper.dim=[u w];
 magnet_lower.dim=[c d];
 
-%magnetisation
+% Magnetisation
 magnet_upper.magn=B1;
 magnet_lower.magn=-B2;                          %IMPORTANT, change for repulsion(-) or attraction
 
-%Displacement
-displ=[0; 0; 1]*linspace(-.2,.2,100);
+% Displacement
+N=10;              %number of calculation steps
+offset=repmat([0;0;0.03],[1 N]);
+displ=linspace(0,.4,N);
+displ_range=offset+[0;0;1]*displ;
 
-%calculate forces
-forces=magnetforces(magnet_upper,magnet_lower,displ);
+% Calculate forces
+forces=magnetforces(magnet_upper,magnet_lower,displ_range);
 
-%Plot output
-plot(displ,forces(3,:))
+% Plot output
+plot((displ),forces(3,:))
 title('Force between two cylindrical magnets');
 xlabel('Displacement, m');
 ylabel('Force, N');
-%% Numerical solution
 
+% Numerical solution
+for i=1:N-1
+    x=.4/N*i;           % Variable distance between magnets
 A_str=num2str(a);
 B_str=num2str(b);
 C_str=num2str(c);
@@ -53,12 +62,12 @@ Mu_str=num2str(mu);
 B1_str=num2str(B1);
 B2_str=num2str(B2);
 H1_str=num2str(H1);
-H2_str=num2str(-H2);                %IMPORTANT, change for repulsion(-) or attraction
+H2_str=num2str(-H2);                % IMPORTANT, change for repulsion(-) or attraction
 MESH_str=num2str(mesh);
 semi='''';
 
 
-%print ansys batch code to text file
+% Print ansys batch code to text file total.txt
 fid1=fopen('total.txt','wt');
 fprintf(fid1,'/PREP7\n');
 fprintf(fid1,'/TITLE,numerical solution\n');
@@ -70,7 +79,7 @@ fprintf(fid1,'ET,1,PLANE53\n');
 fprintf(fid1,'KEYOPT,1,3,1\n');
 fprintf(fid1,'MP,MURX,1,1\n');
 fprintf(fid1,'MP,MURX,2,');
-fprintf(fid1,'%f',B2);
+fprintf(fid1,'%s',B2_str);
 fprintf(fid1,'\n');
 fprintf(fid1,'MP,MGYY,2,');
 fprintf(fid1,'%s',H1_str);
@@ -173,17 +182,35 @@ fprintf(fid1,',');
 fprintf(fid1,'%s',semi);
 fprintf(fid1,'txt');
 fprintf(fid1,'%s',semi);
-fprintf(fid1,',,\n');
-%fprintf(fid1,',,APPEND\n'); %changes with previous line to append to output
+%fprintf(fid1,',,\n');
+fprintf(fid1,',,APPEND\n'); %changes with previous line to append to output
 
 fprintf(fid1,'*VWRITE,ff(1),ff(2),ff(3),ff(4)\n');
 fprintf(fid1,'(E10.3)\n');
 fprintf(fid1,'/OUTPUT\n');
 fprintf(fid1,'/EOF');
 fclose(fid1);
-%% Run the ANSYS batch file
-%!"C:\Program Files\ANSYS Inc\v140\ansys\bin\intel\ANSYS140" -b -i C:\Code\matlab\total.txt -o C:\Code\\matlab\result.out
 
+
+% Run the ANSYS batch file
+!"C:\Program Files\ANSYS Inc\v140\ansys\bin\intel\ANSYS140" -b -i C:\Code\matlab\total.txt -o C:\Code\\matlab\result.out
+    
+    i=i+1;
+end
+
+% Load ANSYS results    
 load output.txt;
 Q=output;
 clear output;
+
+
+% Plot ANSYS results
+for k=1:N-1
+    hold on;
+    plot(displ(k),Q((k*4)-1),'g+:');
+    plot(displ(k),Q((k*4)),'b+:');
+    title('numerical results');
+    xlabel('displacement, m');
+    ylabel('Force, N');
+    k=k+1;
+end
