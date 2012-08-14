7a793c9b62cc85cab2729c982fcf12fdc97ce07e
Joost Ziggers <jziggers@mecht-jz1.mecheng.adelaide.edu.au>
2012-08-14 14:58:22 +0930

    - Added comments and some aesthetic changes

--- ansys/Matlab_ansys.m
+++ ansys/Matlab_ansys.m
@@ -3,124 +3,129 @@ clear all; close all; clc;
 
 hold on;
     
-mu=1.05;                        %permeability (N/A^2)
-mesh=0.005;                     %meshsize numerical solver
+mu = 1.05;                                  % Permeability (N/A^2)
+mesh = 0.005;                               % Meshsize numerical solver
 
 % Dimensions [radius height (thickness)]
-magnet_fixed.dim=[0.06 0.03];  % magnet 1
-magnet_float.dim=[0.06 0.03];  % magnet 2
+magnet_fixed.dim = [0.06 0.03];             % Magnet 1
+magnet_float.dim = [0.06 0.03];             % Magnet 2
 
-% Add or remove comment sign in order to use ring magnets
-magnet_fixed2.dim=[.03 .03]; % Used for upper ring magnet
-magnet_float2.dim=[.03 .03]; % Used for lower ring magnet
+% Add or remove comment sign in order to use ring magnets or not
+magnet_fixed2.dim = [.03 .03];              % Used for upper ring magnet
+magnet_float2.dim = [.03 .03];              % Used for lower ring magnet
 
 % Check for existence of magnet dimensions
-A=length(magnet_fixed.dim);
-B=exist('magnet_fixed2','var');
-C=exist('magnet_float2','var');
+A = length(magnet_fixed.dim);
+B = exist('magnet_fixed2','var');
+C = exist('magnet_float2','var');
 
+% Determine magnet configuration
 if A==2 && B==0 && C==0
-    magnettype='axisymmetric';
+    magnettype='axisymmetric';              % Two axisymmetric (cylindrical) magnets
     disp('Matlab: axisymmetric magnets');
 end
 if A==2 && B==1 && C==0
-    magnettype='axiring';
+    magnettype='axiring';                   % An axisymmetric (cylindrical) magnet and a ring magnet
     disp('Matlab: cylinder and ring magnet');
 end
 if A==2 && B==1 && C==1
-    magnettype='rings';
+    magnettype='rings';                     % Two ring magnets
     disp('Matlab: ring magnets');
 end
 if (length(magnet_fixed.dim))==3
-    magnettype='planar';
+    magnettype='planar';                    % Two planar (cubic) magnets
     disp('Matlab: planar magnets');
 end
 
-
-% Magnetisation
-magnet_fixed.magn=1.2;
-magnet_float.magn=-1.2;                 % IMPORTANT, change for repulsion(-) or attraction
-magnet_fixed2.magn=-magnet_fixed.magn;  % Upper, inner magnet used for superposition
-magnet_float2.magn=-magnet_float.magn;  % Lower, inner magnet used for superposition
+% Magnetisation, change for repulsion or attraction
+magnet_fixed.magn = 1.2;                    % Upper magnet
+magnet_float.magn = -1.2;                   % Lower magnet
+magnet_fixed2.magn = -magnet_fixed.magn;    % Upper, inner magnet used for superposition for ring magnet
+magnet_float2.magn = -magnet_float.magn;    % Lower, inner magnet used for superposition for ring magnet
 
 % Displacement
 displ_max = 0.05;
-N=10;                           % Number of calculation steps
+N = 10;                                     % Number of calculation steps
             switch magnettype
                 case 'axisymmetric'
-                    offset=repmat([0;0;0.03],[1 N*10]);
-                    displ=linspace(0,displ_max,N*10);
-                    displ_range=offset+[0;0;1]*displ;
+                    offset = repmat([0;0;0.03],[1 N*10]);
+                    displ = linspace(0,displ_max,N*10);
+                    displ_range = offset+[0;0;1]*displ;
                     % Calculate forces
-                    forces=magnetforces(magnet_fixed,magnet_float,displ_range);
+                    forces = magnetforces(magnet_fixed,magnet_float,displ_range);
                     % Plot output
-                    plot((displ),forces(3,:))
+                    plot((displ),forces(3,:),'k')
                     title('Force between two cylindrical magnets');
                     xlabel('Displacement, m');
                     ylabel('Force, N');
                     %ansys(N,displ,mu,mesh,magnet_fixed,magnet_float);
                 case 'planar'
-                    magnet_fixed.magdir=[0 1 0];
-                    magnet_float.magdir=[0 1 0];
-                    offset=repmat([0;0.03;0],[1 N*10]);
-                    displ=linspace(eps,displ_max,N*10);
-                    displ_range=offset+[0;1;0]*displ;
+                    magnet_fixed.magdir = [0 1 0];
+                    magnet_float.magdir = [0 1 0];
+                    offset = repmat([0;0.03;0],[1 N*10]);
+                    displ = linspace(eps,displ_max,N*10);
+                    displ_range = offset+[0;1;0]*displ;
                     % Calculate forces
-                    forces=magnetforces(magnet_fixed,magnet_float,displ_range);
+                    forces = magnetforces(magnet_fixed,magnet_float,displ_range);
                     % Plot output
-                    plot((displ),forces(2,:))
+                    plot((displ),forces(2,:),'k')
                     title('Force between two planar magnets');
                     xlabel('Displacement, m');
                     ylabel('Force, N');
                     %ansys(N,displ,mu,mesh,magnet_fixed,magnet_float);
                 case 'axiring'
-                    offset=repmat([0;0;0.03],[1 N*10]);
-                    displ=linspace(eps,displ_max,N*10);
-                    displ_range=offset+[0;0;1]*displ; 
+                    offset = repmat([0;0;0.03],[1 N*10]);
+                    displ = linspace(eps,displ_max,N*10);
+                    displ_range = offset+[0;0;1]*displ; 
                     % Calculate forces
-                    forces=magnetforces(magnet_fixed,magnet_float,displ_range)+magnetforces(magnet_fixed2,magnet_float,displ_range);
+                    forces = magnetforces(magnet_fixed,magnet_float,displ_range)+magnetforces(magnet_fixed2,magnet_float,displ_range);
                     % Plot output
-                    plot((displ),forces(3,:))
+                    plot((displ),forces(3,:),'k')
                     title('Force between cylinder and ring magnet');
                     xlabel('Displacement, m');
                     ylabel('Force, N');
                     %ansys(N,displ,mu,mesh,magnet_fixed,magnet_float,magnet_fixed2);
                 case 'rings'
-                    offset=repmat([0;0;0.03],[1 N*10]);
-                    displ=linspace(0,displ_max,N*10);
-                    displ_range=offset+[0;0;1]*displ; 
+                    offset = repmat([0;0;0.03],[1 N*10]);
+                    displ = linspace(0,displ_max,N*10);
+                    displ_range = offset+[0;0;1]*displ; 
                     % Calculate forces
-                    forces=magnetforces(magnet_fixed,magnet_float,displ_range)+magnetforces(magnet_fixed2,magnet_float,displ_range)+magnetforces(magnet_fixed,magnet_float2,displ_range)+magnetforces(magnet_fixed2,magnet_float2,displ_range);
-                    plot((displ),forces(3,:))
+                    forces = magnetforces(magnet_fixed,magnet_float,displ_range)+magnetforces(magnet_fixed2,magnet_float,displ_range)+magnetforces(magnet_fixed,magnet_float2,displ_range)+magnetforces(magnet_fixed2,magnet_float2,displ_range);
+                    plot((displ),forces(3,:),'k')
                     title('Force between ring magnets');
                     xlabel('Displacement, m');
                     ylabel('Force, N');
-                    ansys(N,displ,mu,mesh,magnet_fixed,magnet_float,magnet_fixed2,magnet_float2);
+                    %ansys(N,displ,mu,mesh,magnet_fixed,magnet_float,magnet_fixed2,magnet_float2);
                 otherwise
             end
 
 % Load ANSYS results    
 load output.txt;
-Q=output;
+Q = output;
 clear output;
 
-Qmx = Q(3:4:end);
-Qvw = Q(4:4:end);
+% Select correct Ansys values
+Qmx = Q(3:4:end);                               % Maxwell force
+Qvw = Q(4:4:end);                               % Virtual work force
 ansys_displ = linspace(0,displ_max,numel(Qvw));
 
 % Plot ANSYS results
 switch magnettype
     case 'axisymmetric'
         plot(ansys_displ,Qmx,'g+:');
-        plot(ansys_displ,Qvw,'b+:');
+        plot(ansys_displ,Qvw,'r+:');
+        legend('Matlab','Maxwell','Virtual work');
     case 'planar'
         plot(ansys_displ,Qmx*magnet_fixed.dim(1,3),'g+:');
-        plot(ansys_displ,Qvw*magnet_fixed.dim(1,3),'b+:');
+        plot(ansys_displ,Qvw*magnet_fixed.dim(1,3),'r+:');
+        legend('Matlab','Maxwell','Virtual work');
     case 'axiring'
         plot(ansys_displ,Qmx,'g+:');
-        plot(ansys_displ,Qvw,'b+:');
+        plot(ansys_displ,Qvw,'r+:');
+        legend('Matlab','Maxwell','Virtual work');
     case 'rings'
         plot(ansys_displ,Qmx,'g+:');
-        plot(ansys_displ,Qvw,'b+:');
+        plot(ansys_displ,Qvw,'r+:');
+        legend('Matlab','Maxwell','Virtual work');
     otherwise
 end
--- ansys/ansys.m
+++ ansys/ansys.m
@@ -4,25 +4,24 @@ function []=ansys(N,displ,mu,mesh,magnet_fixed,magnet_float,magnet_fixed2,magnet
 !del total.txt
 
 %% Determine magnettype
-A=length(magnet_fixed.dim);
-B=exist('magnet_fixed2','var');
-C=exist('magnet_float2','var');
+A = length(magnet_fixed.dim);
+B = exist('magnet_fixed2','var');
+C = exist('magnet_float2','var');
 
 if A==2 && B==0 && C==0
-    magnettype='axisymmetric';
+    magnettype='axisymmetric';                  % Two axisymmetric (cylindrical) magnets
     disp('Ansys: axisymmetric magnets');
 end
 if A==2 && B==1 && C==0
-    magnettype='axiring';
-
+    magnettype='axiring';                       % An axisymmetric (cylindrical) magnet and a ring magnet
     disp('Ansys: cylinder and ring magnet');
 end
 if A==2 && B==1 && C==1
-    magnettype='rings';
+    magnettype='rings';                         % Two ring magnets
     disp('Ansys: ring magnets');
 end
 if A==3
-    magnettype='planar';
+    magnettype='planar';                        % Two planar (cubic) magnets
     disp('Ansys: planar magnets');
 end
 
@@ -32,52 +31,54 @@ end
     % Upper (fixed) magnet is magnet 1
     % Lower (float) magnet is magnet 2
  
-    g=max(displ)-min(displ);
-    x=min(displ)+g/N*i;             % Variable distance between magnets
-    disp(x);
-    mu0=4*pi*10^-7;                 %permeability of space (N/A^2)
-    h1=magnet_fixed.magn/(mu*mu0);  %coercivity magnet 1 (A/m)
-    h2=magnet_float.magn/(mu*mu0);  %coercivity magnet 2 (A/m)
+    g = max(displ)-min(displ);
+    x = min(displ)+g/N*i;                       % Variable distance between magnets
+    disp(x);                                    % Display current displacement calculation
+    mu0 = 4*pi*10^-7;                           % Permeability of space (N/A^2)
+    h1 = magnet_fixed.magn/(mu*mu0);            % Coercivity magnet 1 (A/m)
+    h2 = magnet_float.magn/(mu*mu0);            % Coercivity magnet 2 (A/m)
 
-    b=(magnet_fixed.dim(1,2)+magnet_float.dim(1,2))*10;     % Air gap height, equals total magnet height*10
-    a=magnet_float.dim(1,1)*10;                                                  % Air gap width
-    c=magnet_float.dim(1,1);        % Lower magnet (outer) radius
-    d=magnet_float.dim(1,2);        % Lower magnet height
-    u=magnet_fixed.dim(1,1);        % Upper magnet (outer) radius
-    e=b/2-(x/2)-d;                  % Distance from bottom of airgap to bottom of lower magnet
-    ed=e+d;                         % Distance from bottom of airgap to top of lower magner
-    w=magnet_fixed.dim(1,2);        % Upper magnet height
-    edx=e+d+x;                      % Distance from bottom of airgap to bottom of upper magnet
-    edxw=e+d+x+w;                   % Distance from bottom of airgap to top of upper magnet
+    b = (magnet_fixed.dim(1,2)+magnet_float.dim(1,2))*10; % Air gap height, equals total magnet height*10
+    a = magnet_float.dim(1,1)*10;               % Air gap width
+    c = magnet_float.dim(1,1);                  % Lower magnet (outer) radius
+    d = magnet_float.dim(1,2);                  % Lower magnet height
+    u = magnet_fixed.dim(1,1);                  % Upper magnet (outer) radius
+    e = b/2-(x/2)-d;                            % Distance from bottom of airgap to bottom of lower magnet
+    ed = e+d;                                   % Distance from bottom of airgap to top of lower magner
+    w = magnet_fixed.dim(1,2);                  % Upper magnet height
+    edx = e+d+x;                                % Distance from bottom of airgap to bottom of upper magnet
+    edxw = e+d+x+w;                             % Distance from bottom of airgap to top of upper magnet
 
-    b1=magnet_fixed.magn;
-    b2=magnet_float.magn*-1;
-    semi='''';
+    b1 = magnet_fixed.magn;
+    b2 = magnet_float.magn*-1;
+    semi = '''';                                
     
-    fid1=fopen('total.txt','wt');
+    % Analysis settings
+    fid1 = fopen('total.txt','wt');
     fprintf(fid1,'/PREP7\n');
     fprintf(fid1,'/TITLE,numerical solution\n');
-    %fprintf(fid1,'l=ARG1\n');
+    %fprintf(fid1,'l=ARG1\n');                  % Not needed? Check for all configurations. not needed for: rings,
     fprintf(fid1,'KEYW,PR_SET,1\n');
     fprintf(fid1,'KEYW,PR_ELMAG,1\n');
     fprintf(fid1,'KEYW,MAGNOD,1\n');
-    fprintf(fid1,'ET,1,PLANE53\n');
+    fprintf(fid1,'ET,1,PLANE53\n');             % Mesh type
        
         switch magnettype
             case 'axisymmetric'
                 fprintf(fid1,'KEYOPT,1,3,1\n');
             case 'planar'
-                f=a/2-c/2;          % Distance from left side to left side lower magnet
-                h=a/2-u/2;          % Distance from left side to left side upper magnet
+                f=a/2-c/2;                      % Distance from left side to left side lower magnet
+                h=a/2-u/2;                      % Distance from left side to left side upper magnet
             case 'axiring'
                 fprintf(fid1,'KEYOPT,1,3,1\n');
-                h=magnet_fixed2.dim(1,1); % Distance from left side to inner radius upper magnet
+                h=magnet_fixed2.dim(1,1);       % Distance from left side to inner radius upper magnet
             case 'rings'
                 fprintf(fid1,'KEYOPT,1,3,1\n');
-                h=magnet_fixed2.dim(1,1); % Distance from left side to inner radius upper magnet
-                j=magnet_float2.dim(1,1); % Distance from left side to inner radius lower magnet
+                h=magnet_fixed2.dim(1,1);       % Distance from left side to inner radius upper magnet
+                j=magnet_float2.dim(1,1);       % Distance from left side to inner radius lower magnet
         end
-
+    
+    % Input of material properties and creation of surrounding air
     fprintf(fid1,'MP,MURX,1,1\n');
     fprintf(fid1,'MP,MURX,2,');
     fprintf(fid1,'%f',b2);
@@ -97,6 +98,7 @@ end
     fprintf(fid1,'%f',b);
     fprintf(fid1,'\n');
     
+        % Create different types of magnets
         switch magnettype
             case 'axisymmetric'
             fprintf(fid1,'RECTNG,0,');
@@ -170,6 +172,7 @@ end
             fprintf(fid1,'\n');
         end
     
+    % Create areas and assign material properties
     fprintf(fid1,'ASEL,S,AREA,,2\n');
     fprintf(fid1,'AATT,2,,1\n');
     fprintf(fid1,'ASEL,S,AREA,,3\n');
@@ -179,17 +182,20 @@ end
     fprintf(fid1,'ASBA,1,ALL,,,KEEP\n');
     fprintf(fid1,'ASEL,U,AREA,,2,3\n');
     fprintf(fid1,'AATT,1,,1\n');
-    %boundary conditions
+    
+    % Boundary conditions
     fprintf(fid1,'ALLSEL\n');
     fprintf(fid1,'LSEL,S,EXT\n');
     fprintf(fid1,'DL,ALL,,AZ,0\n');
-    %mesh
+    
+    % Mesh
     fprintf(fid1,'ALLSES\n');
     fprintf(fid1,'AESIZE,ALL,');
     fprintf(fid1,'%f',mesh);
     fprintf(fid1,'\n');
     fprintf(fid1,'AMESH,ALL\n');
-    %apply force flags
+    
+    % Apply force flags
     fprintf(fid1,'ESEL,S,MAT,,3\n');
     fprintf(fid1,'CM,mag1,ELEM\n');
     fprintf(fid1,'FMAGBC,');
@@ -236,14 +242,14 @@ end
 
     fprintf(fid1,'/OUTPUT,');
     fprintf(fid1,'%s',semi);
-    fprintf(fid1,'output'); %name can be changed
+    fprintf(fid1,'output');                     % Name can be changed
     fprintf(fid1,'%s',semi);
     fprintf(fid1,',');
     fprintf(fid1,'%s',semi);
     fprintf(fid1,'txt');
     fprintf(fid1,'%s',semi);
     %fprintf(fid1,',,\n');
-    fprintf(fid1,',,APPEND\n'); %changes with previous line to append to output
+    fprintf(fid1,',,APPEND\n');                 % Changes with previous line to append to output
 
     fprintf(fid1,'*VWRITE,ff(1),ff(2),ff(3),ff(4)\n');
     fprintf(fid1,'(E10.3)\n');
@@ -252,12 +258,12 @@ end
     fclose(fid1);
 
 
-    % Run the ANSYS batch file
+    % Run the ANSYS batch file, locations must be correct
     !"C:\Program Files\ANSYS Inc\v140\ansys\bin\intel\ANSYS140" -b -i C:\magcode\ansys\total.txt -o C:\magcode\ansys\result.out
 
         i=i+1;
     end
-    %delete 'unnecessary' files
+    % Delete 'unnecessary' files
     !del file.BCS
     !del file.emat
     !del file.err
