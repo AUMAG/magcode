1c4d56209adcad4b241dddd5decaf77e90aa1213
Joost Ziggers <jziggers@mecht-jz1.mecheng.adelaide.edu.au>
2012-08-14 10:46:49 +0930

    Script for both axisymmetric and planar magnet calculations in Matlab and Ansys

--- ansys/Matlab_ansys.m
+++ ansys/Matlab_ansys.m
@@ -1,216 +1,87 @@
-%%This script compares analytical Matlab with numerical ANSYS results
+%% This script compares analytical results with numerical results
+clear all; close all; clc;
 
-clear all;close all;clc;
-!del output.txt
-!del total.txt
-% Magnet 1 is upper magnet
-a=0.3;          %air gap width
-b=0.6;          %air gap height
-c=0.05;         %radius magnet 2
-e=0.14;         %distance between magnet 2 and bottom of airgap
-d=0.03;         %height of magnet 2
-%x=0.03;        %distance between 2 magnets, not needed in this script
-w=0.03;         %height of magnet 1
-u=0.035;        %raduis magnet 1
-B1=1.2;         %magnetisation magnet 1 (T)
-B2=1.2;         %magnetisation magnet 2(T)
-mu=1.05;        %permeability (N/A^2)
-mu0=4*pi*10^-7; %permeability of space (N/A^2)
-H1=B1/(mu*mu0); %coercivity magnet 1 (A/m)
-H2=B2/(mu*mu0); %coercivity magnet 2 (A/m)
-mesh=0.005;     %meshsize numerical solver
+figure(77)
+hold on;
+    
+mu=1.05;                        %permeability (N/A^2)
+mesh=0.005;                     %meshsize numerical solver
 
-%% analytical solution
-% Dimensions [radius height]
-magnet_upper.dim=[u w];
-magnet_lower.dim=[c d];
+% Dimensions [radius height (thickness)]
+magnet_fixed.dim=[0.035 0.03 .1];  % magnet 1
+magnet_float.dim=[0.05  0.03 .1];  % magnet 2
+
+if (length(magnet_fixed.dim))==2
+    magnettype='axisymmetric';
+     disp('Matlab: axisymmetric magnets');
+end
+if (length(magnet_fixed.dim))==3
+    magnettype='planar';
+    disp('Matlab: planar magnets');
+end
+
 
 % Magnetisation
-magnet_upper.magn=B1;
-magnet_lower.magn=-B2;                          %IMPORTANT, change for repulsion(-) or attraction
+magnet_fixed.magn=1.2;
+magnet_float.magn=-1.2;         %IMPORTANT, change for repulsion(-) or attraction
 
 % Displacement
-N=10;              %number of calculation steps
-offset=repmat([0;0;0.03],[1 N]);
-displ=linspace(0,.4,N);
-displ_range=offset+[0;0;1]*displ;
+displ_max = 0.05;
+N=10;                           %number of calculation steps
+            switch magnettype
+                case 'axisymmetric'
+                    offset=repmat([0;0;0.03],[1 N]);
+                    displ=linspace(0,.3,N);
+                    displ_range=offset+[0;0;1]*displ; 
+                case 'planar'
+                    magnet_fixed.magdir=[0 1 0];
+                    magnet_float.magdir=[0 1 0];
+                    offset=repmat([0;0.03;0],[1 N]);
+                    displ=linspace(eps,displ_max,N);
+                    displ_range=offset+[0;1;0]*displ;
+                otherwise
+            end
 
 % Calculate forces
-forces=magnetforces(magnet_upper,magnet_lower,displ_range);
+forces=magnetforces(magnet_fixed,magnet_float,displ_range);
 
-% Plot output
-plot((displ),forces(3,:))
-title('Force between two cylindrical magnets');
-xlabel('Displacement, m');
-ylabel('Force, N');
+        switch magnettype
+            case 'axisymmetric'
+                % Plot output
+                plot((displ),forces(3,:))
+                title('Force between two cylindrical magnets');
+                xlabel('Displacement, m');
+                ylabel('Force, N');
+            case 'planar'
+                % Plot output
+                plot((displ),forces(2,:))
+                title('Force between two planar magnets');
+                xlabel('Displacement, m');
+                ylabel('Force, N');
+            otherwise
+        end
 
-% Numerical solution
-for i=1:N-1
-    x=.4/N*i;           % Variable distance between magnets
-A_str=num2str(a);
-B_str=num2str(b);
-C_str=num2str(c);
-D_str=num2str(d);
-E_str=num2str(e);
-ED_str=num2str(e+d);
-X_str=num2str(x);
-W_str=num2str(w);
-EDX_str=num2str(e+d+x);
-EDXW_str=num2str(e+d+x+w);
-U_str=num2str(u);
-Mu_str=num2str(mu);
-B1_str=num2str(B1);
-B2_str=num2str(B2);
-H1_str=num2str(H1);
-H2_str=num2str(-H2);                % IMPORTANT, change for repulsion(-) or attraction
-MESH_str=num2str(mesh);
-semi='''';
-
-
-% Print ansys batch code to text file total.txt
-fid1=fopen('total.txt','wt');
-fprintf(fid1,'/PREP7\n');
-fprintf(fid1,'/TITLE,numerical solution\n');
-fprintf(fid1,'l=ARG1\n');
-fprintf(fid1,'KEYW,PR_SET,1\n');
-fprintf(fid1,'KEYW,PR_ELMAG,1\n');
-fprintf(fid1,'KEYW,MAGNOD,1\n');
-fprintf(fid1,'ET,1,PLANE53\n');
-fprintf(fid1,'KEYOPT,1,3,1\n');
-fprintf(fid1,'MP,MURX,1,1\n');
-fprintf(fid1,'MP,MURX,2,');
-fprintf(fid1,'%s',B2_str);
-fprintf(fid1,'\n');
-fprintf(fid1,'MP,MGYY,2,');
-fprintf(fid1,'%s',H1_str);
-fprintf(fid1,'\n');
-fprintf(fid1,'MP,MURX,3,');
-fprintf(fid1,'%s',B1_str);
-fprintf(fid1,'\n');
-fprintf(fid1,'MP,MGYY,3,');
-fprintf(fid1,'%s',H2_str);
-fprintf(fid1,'\n');
-fprintf(fid1,'RECTNG,0,');
-fprintf(fid1,'%s',A_str);
-fprintf(fid1,',0,');
-fprintf(fid1,'%s',B_str);
-fprintf(fid1,'\n');
-fprintf(fid1,'RECTNG,0,');  %+l?
-fprintf(fid1,'%s',C_str);
-fprintf(fid1,',');
-fprintf(fid1,'%s',E_str);
-fprintf(fid1,',');
-fprintf(fid1,'%s',ED_str);
-fprintf(fid1,'\n');
-fprintf(fid1,'RECTNG,0,');
-fprintf(fid1,'%s',U_str);
-fprintf(fid1,',');
-fprintf(fid1,'%s',EDX_str);
-fprintf(fid1,',');
-fprintf(fid1,'%s',EDXW_str);
-fprintf(fid1,'\n');
-
-fprintf(fid1,'ASEL,S,AREA,,2\n');
-fprintf(fid1,'AATT,2,,1\n');
-fprintf(fid1,'ASEL,S,AREA,,3\n');
-fprintf(fid1,'AATT,3,,1\n');
-
-fprintf(fid1,'ASEL,ALL\n');
-fprintf(fid1,'ASBA,1,ALL,,,KEEP\n');
-fprintf(fid1,'ASEL,U,AREA,,2,3\n');
-fprintf(fid1,'AATT,1,,1\n');
-%boundary conditions
-fprintf(fid1,'ALLSEL\n');
-fprintf(fid1,'LSEL,S,EXT\n');
-fprintf(fid1,'DL,ALL,,AZ,0\n');
-%mesh
-fprintf(fid1,'ALLSES\n');
-fprintf(fid1,'AESIZE,ALL,');
-fprintf(fid1,'%s',MESH_str);
-fprintf(fid1,'\n');
-fprintf(fid1,'AMESH,ALL\n');
-%apply force flags
-fprintf(fid1,'ESEL,S,MAT,,3\n');
-fprintf(fid1,'CM,mag1,ELEM\n');
-fprintf(fid1,'FMAGBC,');
-fprintf(fid1,'%s',semi);
-fprintf(fid1,'mag1');
-fprintf(fid1,'%s',semi);
-fprintf(fid1,'\n');
-fprintf(fid1,'PRIM\n');
-
-fprintf(fid1,'FINISH\n');
-fprintf(fid1,'ALLSEL\n');
-fprintf(fid1,'/SOL\n');
-fprintf(fid1,'MAGSOLV,2\n');
-fprintf(fid1,'FINISH\n');
-
-fprintf(fid1,'/POST1\n');
-fprintf(fid1,'PLF2D\n');
-fprintf(fid1,'fmagsum,');
-fprintf(fid1,'%s',semi);
-fprintf(fid1,'mag1');
-fprintf(fid1,'%s',semi);
-fprintf(fid1,'\n');
-
-fprintf(fid1,'ESEL,S,MAT,,3\n');
-fprintf(fid1,'NSEL,S,EXT\n');
-fprintf(fid1,'ESLN\n');
-fprintf(fid1,'ESEL,U,MAT,,3\n');
-fprintf(fid1,'SSUM\n');
-
-fprintf(fid1,'etab,fmgx1,fmag,x\n');
-fprintf(fid1,'etab,fmgy1,fmag,y\n');
-fprintf(fid1,'etab.fmgz1,fmag,z\n');
-
-fprintf(fid1,'SSUM\n');
-
-fprintf(fid1,'*dim,ff,ARRAY,4\n');
-fprintf(fid1,'*get,ff(1),ssum,fmx_x\n');
-fprintf(fid1,'*get,ff(2),ssum,fvw_x\n');
-fprintf(fid1,'*get,ff(3),ssum,fmx_y\n');
-fprintf(fid1,'*get,ff(4),ssum,fvw_y\n');
-
-fprintf(fid1,'/PAGE,5000,5000,5000,5000\n');
-fprintf(fid1,'/HEADER,OFF,OFF,OFF,OFF,OFF,OFF\n');
-
-fprintf(fid1,'/OUTPUT,');
-fprintf(fid1,'%s',semi);
-fprintf(fid1,'output'); %name can be changed
-fprintf(fid1,'%s',semi);
-fprintf(fid1,',');
-fprintf(fid1,'%s',semi);
-fprintf(fid1,'txt');
-fprintf(fid1,'%s',semi);
-%fprintf(fid1,',,\n');
-fprintf(fid1,',,APPEND\n'); %changes with previous line to append to output
-
-fprintf(fid1,'*VWRITE,ff(1),ff(2),ff(3),ff(4)\n');
-fprintf(fid1,'(E10.3)\n');
-fprintf(fid1,'/OUTPUT\n');
-fprintf(fid1,'/EOF');
-fclose(fid1);
-
-
-% Run the ANSYS batch file
-!"C:\Program Files\ANSYS Inc\v140\ansys\bin\intel\ANSYS140" -b -i C:\Code\matlab\total.txt -o C:\Code\\matlab\result.out
-    
-    i=i+1;
-end
+% Calculate using Ansys
+ansys(N,displ,mu,mesh,magnet_fixed,magnet_float);
 
 % Load ANSYS results    
 load output.txt;
 Q=output;
 clear output;
 
+Qvw = Q(3:4:end);
+Qmx = Q(4:4:end);
+ansys_displ = linspace(0,displ_max,numel(Qvw));
 
 % Plot ANSYS results
-for k=1:N-1
-    hold on;
-    plot(displ(k),Q((k*4)-1),'g+:');
-    plot(displ(k),Q((k*4)),'b+:');
-    title('numerical results');
-    xlabel('displacement, m');
-    ylabel('Force, N');
-    k=k+1;
+switch magnettype
+    case 'axisymmetric'
+        plot(ansys_displ,Qvw,'g+:');
+        plot(ansys_displ,Qmx,'b+:');
+        
+    case 'planar'
+        plot(ansys_displ,Qvw*magnet_fixed.dim(1,3),'g+:');
+        plot(ansys_displ,Qmx*magnet_fixed.dim(1,3),'b+:');
+        
+    otherwise
 end
---
+++ ansys/ansys.m
@@ -0,0 +1,222 @@
+function []=ansys(N,displ,mu,mesh,magnet_fixed,magnet_float)
+% Numerical solution of two magnets
+!del output.txt
+!del total.txt
+
+%% Determine magnettype
+if (length(magnet_fixed.dim))==2
+    magnettype='axisymmetric';
+end
+if (length(magnet_fixed.dim))==3
+    magnettype='planar';
+end
+
+        switch magnettype
+            case 'axisymmetric'
+                disp('Ansys: axisymmetric magnets');
+
+            case 'planar'
+                disp('Ansys: planar magnets');
+                
+            otherwise
+                disp('Unknown magnet type. Change dimensions');
+        end
+        
+%% Write Ansys code
+    for i=0:N
+    % Upper (fixed) magnet is magnet 1
+    % Lower (float) magnet is magnet 2
+ 
+    g=max(displ)-min(displ);
+    x=min(displ)+g/N*i;             % Variable distance between magnets
+    disp(x);
+    mu0=4*pi*10^-7;                 %permeability of space (N/A^2)
+    h1=magnet_fixed.magn/(mu*mu0);  %coercivity magnet 1 (A/m)
+    h2=magnet_float.magn/(mu*mu0);  %coercivity magnet 2 (A/m)
+
+    b=(magnet_fixed.dim(1,2)+magnet_float.dim(1,2))*10;     % Air gap height, equals total magnet height*10
+    a=magnet_float.dim(1,1)*10;                                                  % Air gap width
+    c=magnet_float.dim(1,1);        % Lower magnet radius
+    d=magnet_float.dim(1,2);        % Lower magnet height
+    u=magnet_fixed.dim(1,1);        % Upper magnet radius
+    e=b/2-(x/2)-d;                  % Distance from bottom of airgap to bottom of lower magnet
+    ed=e+d;                         % Distance from bottom of airgap to top of lower magner
+    w=magnet_fixed.dim(1,2);        % Upper magnet height
+    edx=e+d+x;                      % Distance from bottom of airgap to bottom of upper magnet
+    edxw=e+d+x+w;                   % Distance from bottom of airgap to top of upper magnet
+
+    b1=magnet_fixed.magn;
+    b2=magnet_float.magn*-1;
+    semi='''';
+    
+    fid1=fopen('total.txt','wt');
+    fprintf(fid1,'/PREP7\n');
+    fprintf(fid1,'/TITLE,numerical solution\n');
+    fprintf(fid1,'l=ARG1\n');
+    fprintf(fid1,'KEYW,PR_SET,1\n');
+    fprintf(fid1,'KEYW,PR_ELMAG,1\n');
+    fprintf(fid1,'KEYW,MAGNOD,1\n');
+    fprintf(fid1,'ET,1,PLANE53\n');
+       
+        switch magnettype
+            case 'axisymmetric'
+                fprintf(fid1,'KEYOPT,1,3,1\n');
+            case 'planar'
+                
+                f=a/2-c/2;          % Distance from left side to left side lower magnet
+                h=a/2-u/2;          % Distance from left side to left side upper magnet
+
+        end
+
+    fprintf(fid1,'MP,MURX,1,1\n');
+    fprintf(fid1,'MP,MURX,2,');
+    fprintf(fid1,'%f',b2);
+    fprintf(fid1,'\n');
+    fprintf(fid1,'MP,MGYY,2,');
+    fprintf(fid1,'%f',h1);
+    fprintf(fid1,'\n');
+    fprintf(fid1,'MP,MURX,3,');
+    fprintf(fid1,'%f',b1);
+    fprintf(fid1,'\n');
+    fprintf(fid1,'MP,MGYY,3,');            
+    fprintf(fid1,'%f',h2);
+    fprintf(fid1,'\n');
+    fprintf(fid1,'RECTNG,0,');
+    fprintf(fid1,'%f',a);
+    fprintf(fid1,',0,');
+    fprintf(fid1,'%f',b);
+    fprintf(fid1,'\n');
+    
+        switch magnettype
+            case 'axisymmetric'
+            fprintf(fid1,'RECTNG,0,');
+            fprintf(fid1,'%f',c);
+            fprintf(fid1,',');
+            fprintf(fid1,'%f',e);
+            fprintf(fid1,',');
+            fprintf(fid1,'%f',ed);
+            fprintf(fid1,'\n');
+            fprintf(fid1,'RECTNG,0,');
+            fprintf(fid1,'%f',u);
+            fprintf(fid1,',');
+            fprintf(fid1,'%f',edx);
+            fprintf(fid1,',');
+            fprintf(fid1,'%f',edxw);
+            fprintf(fid1,'\n');   
+            case 'planar'
+            fprintf(fid1,'RECTNG,');
+            fprintf(fid1,'%f',f);
+            fprintf(fid1,',');
+            fprintf(fid1,'%f',f+c);
+            fprintf(fid1,',');
+            fprintf(fid1,'%f',e);
+            fprintf(fid1,',');
+            fprintf(fid1,'%f',ed);
+            fprintf(fid1,'\n');
+            fprintf(fid1,'RECTNG,');
+            fprintf(fid1,'%f',h);
+            fprintf(fid1,',');
+            fprintf(fid1,'%f',h+u);
+            fprintf(fid1,',');
+            fprintf(fid1,'%f',edx);
+            fprintf(fid1,',');
+            fprintf(fid1,'%f',edxw);
+            fprintf(fid1,'\n');  
+        end
+    
+    fprintf(fid1,'ASEL,S,AREA,,2\n');
+    fprintf(fid1,'AATT,2,,1\n');
+    fprintf(fid1,'ASEL,S,AREA,,3\n');
+    fprintf(fid1,'AATT,3,,1\n');
+
+    fprintf(fid1,'ASEL,ALL\n');
+    fprintf(fid1,'ASBA,1,ALL,,,KEEP\n');
+    fprintf(fid1,'ASEL,U,AREA,,2,3\n');
+    fprintf(fid1,'AATT,1,,1\n');
+    %boundary conditions
+    fprintf(fid1,'ALLSEL\n');
+    fprintf(fid1,'LSEL,S,EXT\n');
+    fprintf(fid1,'DL,ALL,,AZ,0\n');
+    %mesh
+    fprintf(fid1,'ALLSES\n');
+    fprintf(fid1,'AESIZE,ALL,');
+    fprintf(fid1,'%f',mesh);
+    fprintf(fid1,'\n');
+    fprintf(fid1,'AMESH,ALL\n');
+    %apply force flags
+    fprintf(fid1,'ESEL,S,MAT,,3\n');
+    fprintf(fid1,'CM,mag1,ELEM\n');
+    fprintf(fid1,'FMAGBC,');
+    fprintf(fid1,'%s',semi);
+    fprintf(fid1,'mag1');
+    fprintf(fid1,'%s',semi);
+    fprintf(fid1,'\n');
+    fprintf(fid1,'PRIM\n');
+
+    fprintf(fid1,'FINISH\n');
+    fprintf(fid1,'ALLSEL\n');
+    fprintf(fid1,'/SOL\n');
+    fprintf(fid1,'MAGSOLV,2\n');
+    fprintf(fid1,'FINISH\n');
+
+    fprintf(fid1,'/POST1\n');
+    fprintf(fid1,'PLF2D\n');
+    fprintf(fid1,'fmagsum,');
+    fprintf(fid1,'%s',semi);
+    fprintf(fid1,'mag1');
+    fprintf(fid1,'%s',semi);
+    fprintf(fid1,'\n');
+
+    fprintf(fid1,'ESEL,S,MAT,,3\n');
+    fprintf(fid1,'NSEL,S,EXT\n');
+    fprintf(fid1,'ESLN\n');
+    fprintf(fid1,'ESEL,U,MAT,,3\n');
+    fprintf(fid1,'SSUM\n');
+
+    fprintf(fid1,'etab,fmgx1,fmag,x\n');
+    fprintf(fid1,'etab,fmgy1,fmag,y\n');
+    fprintf(fid1,'etab.fmgz1,fmag,z\n');
+
+    fprintf(fid1,'SSUM\n');
+
+    fprintf(fid1,'*dim,ff,ARRAY,4\n');
+    fprintf(fid1,'*get,ff(1),ssum,fmx_x\n');
+    fprintf(fid1,'*get,ff(2),ssum,fvw_x\n');
+    fprintf(fid1,'*get,ff(3),ssum,fmx_y\n');
+    fprintf(fid1,'*get,ff(4),ssum,fvw_y\n');
+
+    fprintf(fid1,'/PAGE,5000,5000,5000,5000\n');
+    fprintf(fid1,'/HEADER,OFF,OFF,OFF,OFF,OFF,OFF\n');
+
+    fprintf(fid1,'/OUTPUT,');
+    fprintf(fid1,'%s',semi);
+    fprintf(fid1,'output'); %name can be changed
+    fprintf(fid1,'%s',semi);
+    fprintf(fid1,',');
+    fprintf(fid1,'%s',semi);
+    fprintf(fid1,'txt');
+    fprintf(fid1,'%s',semi);
+    %fprintf(fid1,',,\n');
+    fprintf(fid1,',,APPEND\n'); %changes with previous line to append to output
+
+    fprintf(fid1,'*VWRITE,ff(1),ff(2),ff(3),ff(4)\n');
+    fprintf(fid1,'(E10.3)\n');
+    fprintf(fid1,'/OUTPUT\n');
+    fprintf(fid1,'/EOF');
+    fclose(fid1);
+
+
+    % Run the ANSYS batch file
+    !"C:\Program Files\ANSYS Inc\v140\ansys\bin\intel\ANSYS140" -b -i C:\magcode\ansys\total.txt -o C:\magcode\ansys\result.out
+
+        i=i+1;
+    end
+    %delete 'unnecessary' files
+    !del file.BCS
+    !del file.emat
+    !del file.err
+    !del file.esav
+    !del file.full
+    !del file.rmg
+    !del file.stat
+end
