07f61c85faa3c5991328b2a278f0653735b9938b
Will Robertson <wspr81@gmail.com>
2018-11-29 09:33:38 +1030

    break out cuboid force zz

--- matlab/magnetforces.m
+++ matlab/magnetforces.m
@@ -357,7 +357,7 @@ end
     
     debug_disp('Forces x-x:')
     force_components(1,:) = ...
-      rotate_z_to_x( forces_calc_z_z(size1_x,size2_x,d_x,J1_x,J2_x) );
+      rotate_z_to_x( cuboid_force_z_z(size1_x,size2_x,d_x,J1_x,J2_x) );
     
     debug_disp('Forces x-y:')
     force_components(2,:) = ...
@@ -378,7 +378,7 @@ end
     
     debug_disp('Forces y-y:')
     force_components(5,:) = ...
-      rotate_z_to_y( forces_calc_z_z(size1_y,size2_y,d_y,J1_y,J2_y) );
+      rotate_z_to_y( cuboid_force_z_z(size1_y,size2_y,d_y,J1_y,J2_y) );
     
     debug_disp('Forces y-z:')
     force_components(6,:) = ...
@@ -388,7 +388,7 @@ end
     
     
     debug_disp('z-z force:')
-    force_components(9,:) = forces_calc_z_z( size1,size2,displ,J1,J2 );
+    force_components(9,:) = cuboid_force_z_z( size1,size2,displ,J1,J2 );
     
     debug_disp('z-y force:')
     force_components(8,:) = forces_calc_z_y( size1,size2,displ,J1,J2 );
@@ -548,105 +548,7 @@ end
   end
 
 
-%\begin{mfunction}{forces_calc_z_z}
-%
-%The expressions here follow directly from \textcite{akoun1984}.
-%
-%\begin{center}
-%\begin{tabular}{lll}
-% Inputs:
-% & |size1|=|(a,b,c)| & the half dimensions of the fixed magnet \\
-% & |size2|=|(A,B,C)| & the half dimensions of the floating magnet \\
-% & |displ|=|(dx,dy,dz)| & distance between magnet centres \\
-% &     |(J,J2)| & magnetisations of the magnet in the z-direction \\
-% Outputs:
-% & |forces_xyz|=|(Fx,Fy,Fz)| & Forces of the second magnet \\
-%\end{tabular}
-%\end{center}
 
-  function calc_out = forces_calc_z_z(size1,size2,offset,J1,J2)
-    
-    J1 = J1(3);
-    J2 = J2(3);
-    
-    if (J1==0 || J2==0)
-      debug_disp('Zero magnetisation.')
-      calc_out = [0; 0; 0];
-      return;
-    end
-    
-    component_x = 0;
-    component_y = 0;
-    component_z = 0;
-    
-    count = 0;
-    for ii = [1 -1]
-      for jj = [1 -1]
-        for kk = [1 -1]
-          for ll = [1 -1]
-            for pp = [1 -1]
-              for qq = [1 -1]
-                
-                count = count + 1;
-                
-                u = offset(1) + size2(1)*jj - size1(1)*ii;
-                v = offset(2) + size2(2)*ll - size1(2)*kk;
-                w = offset(3) + size2(3)*qq - size1(3)*pp;
-                r = sqrt(u.^2+v.^2+w.^2);
-                
-                if w == 0
-                  atan_term = 0;
-                else
-                  atan_term = atan(u.*v./(r.*w));
-                end
-                if abs(r-u) < eps
-                  log_ru = 0;
-                else
-                  log_ru = log(r-u);
-                end
-                if abs(r-v) < eps
-                  log_rv = 0;
-                else
-                  log_rv = log(r-v);
-                end
-                
-                cx = ...
-                  + 0.5*(v.^2-w.^2).*log_ru ...
-                  + u.*v.*log_rv ...
-                  + v.*w.*atan_term...
-                  + 0.5*r.*u;
-                
-                cy = ...
-                  + 0.5*(u.^2-w.^2).*log_rv ...
-                  + u.*v.*log_ru ...
-                  + u.*w.*atan_term ...
-                  + 0.5*r.*v;
-                
-                cz = ...
-                  - u.*w.*log_ru ...
-                  - v.*w.*log_rv ...
-                  + u.*v.*atan_term ...
-                  - r.*w;
-
-                ind_sum = ii*jj*kk*ll*pp*qq;
-                component_x = component_x + ind_sum.*cx;
-                component_y = component_y + ind_sum.*cy;
-                component_z = component_z + ind_sum.*cz;
-                
-              end
-            end
-          end
-        end
-      end
-    end
-    
-    calc_out = J1*J2*magconst.*[component_x; component_y; component_z];
-    
-    debug_disp(calc_out')
-    
-  end
-
-%\end{mfunction}
 
 
 
---
+++ matlab/private/cuboid_force_z_z.m
@@ -0,0 +1,98 @@
+%\begin{mfunction}{forces_calc_z_z}
+%
+%The expressions here follow directly from \textcite{akoun1984}.
+%
+%\begin{center}
+%\begin{tabular}{lll}
+% Inputs:
+% & |size1|=|(a,b,c)| & the half dimensions of the fixed magnet \\
+% & |size2|=|(A,B,C)| & the half dimensions of the floating magnet \\
+% & |offset|=|(dx,dy,dz)| & distance between magnet centres \\
+% &     |(J,J2)| & magnetisations of the magnet in the z-direction \\
+% Outputs:
+% & |forces_xyz|=|(Fx,Fy,Fz)| & Forces of the second magnet \\
+%\end{tabular}
+%\end{center}
+
+function forces_xyz = cuboid_force_z_z(size1,size2,offset,J1,J2)
+
+magconst = 1/(4*pi*(4*pi*1e-7));
+
+J1 = J1(3);
+J2 = J2(3);
+
+if (J1==0 || J2==0)
+  forces_xyz = [0; 0; 0];
+  return;
+end
+
+component_x = 0;
+component_y = 0;
+component_z = 0;
+
+count = 0;
+for ii = [1 -1]
+  for jj = [1 -1]
+    for kk = [1 -1]
+      for ll = [1 -1]
+        for pp = [1 -1]
+          for qq = [1 -1]
+            
+            count = count + 1;
+            
+            u = offset(1) + size2(1)*jj - size1(1)*ii;
+            v = offset(2) + size2(2)*ll - size1(2)*kk;
+            w = offset(3) + size2(3)*qq - size1(3)*pp;
+            r = sqrt(u.^2+v.^2+w.^2);
+            
+            if w == 0
+              atan_term = 0;
+            else
+              atan_term = atan(u.*v./(r.*w));
+            end
+            if abs(r-u) < eps
+              log_ru = 0;
+            else
+              log_ru = log(r-u);
+            end
+            if abs(r-v) < eps
+              log_rv = 0;
+            else
+              log_rv = log(r-v);
+            end
+            
+            cx = ...
+              + 0.5*(v.^2-w.^2).*log_ru ...
+              + u.*v.*log_rv ...
+              + v.*w.*atan_term...
+              + 0.5*r.*u;
+            
+            cy = ...
+              + 0.5*(u.^2-w.^2).*log_rv ...
+              + u.*v.*log_ru ...
+              + u.*w.*atan_term ...
+              + 0.5*r.*v;
+            
+            cz = ...
+              - u.*w.*log_ru ...
+              - v.*w.*log_rv ...
+              + u.*v.*atan_term ...
+              - r.*w;
+            
+            ind_sum = ii*jj*kk*ll*pp*qq;
+            component_x = component_x + ind_sum.*cx;
+            component_y = component_y + ind_sum.*cy;
+            component_z = component_z + ind_sum.*cz;
+            
+          end
+        end
+      end
+    end
+  end
+end
+
+forces_xyz = J1*J2*magconst.*[component_x; component_y; component_z];
+
+end
+
+%\end{mfunction}
