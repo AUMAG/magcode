b300173e70b947985b5574f862ae21eadac8710d
Will Robertson <wspr81@gmail.com>
2018-11-29 10:24:12 +1030

    torque and cyl tests still fail, hmmm

--- matlab/testsuite/testcyl01.m
+++ matlab/testsuite/testcyl01.m
@@ -4,23 +4,23 @@ disp('=================')
 fprintf('TEST cylinder forces: ')
 
 magnet_fixed.dim = [0.02 0.04];
-magnet_float.dim = magnet_fixed.dim;
+magnet_float.dim = [0.02 0.04];
 
 magnet_fixed.magn = 1.3;
 magnet_float.magn = 1.3;
 
 magnet_fixed.type = 'cylinder';
-magnet_float.type = magnet_fixed.type;
+magnet_float.type = 'cylinder';
 
 magnet_fixed.dir  = [0 0 1];
 magnet_float.dir  = [0 0 1]; % must be same
 
 magnet_fixed.magdir  = [0 0  1];
-magnet_float.magdir  = [0 0 -1]; % must be aligned
+magnet_float.magdir  = [0 0 -1];
 
 F = magnetforces(magnet_fixed,magnet_float,[0 0 0.05]);
 
-assert( round(1000*F(3)) == 265537 , 'forces between cylindrical magnets' );
+assert( round(1000*F(3)) == 265537 , 'Incorrect reference force between cylindrical magnets' );
 
 fprintf('passed\n')
 disp('=================')
