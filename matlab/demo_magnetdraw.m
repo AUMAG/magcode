020f4612488953c7ac9dc960f4e3f42f33a9734b
Will Robertson <wspr81@gmail.com>
2018-12-03 21:37:24 +1030

    huzzah, magnetdraw is working!

--- matlab/demo_magnetdraw.m
+++ matlab/demo_magnetdraw.m
@@ -2,6 +2,8 @@
 
 function demo_magnetdraw()
 
+clc
+
 %% Plot setup
 
 figure(1); clf; hold on
@@ -9,7 +11,7 @@ view(3);
 axis equal
 camlight
 
-title('Cuboid and cylinder magnet')
+title('Cuboid magnet')
 xlabel('x');
 ylabel('y');
 zlabel('z');
@@ -21,12 +23,6 @@ mag_cuboid = magnetdefine(...
   'type','cuboid',...
   'dim',[0.1 0.2 0.3],...
   'magn',1,'magdir',[0 0 0]);
-
-mag_cuboid.magdir = [5 2 1];
-mag_cuboid.magdir = mag_cuboid.magdir/norm(mag_cuboid.magdir);
-magnetdraw(mag_cuboid,[0; 0; 0]);
-
-return
 
 mag_cuboid.magdir = [0 0 5];
 magnetdraw(mag_cuboid,[-0.1; 0; 0]);
@@ -40,6 +36,22 @@ magnetdraw(mag_cuboid,[-0.5; 0; 0]);
 mag_cuboid.magdir = [5 0 1];
 magnetdraw(mag_cuboid,[-0.7; 0; 0]);
 
+mag_cuboid.magdir = [5 2 1];
+mag_cuboid.magdir = mag_cuboid.magdir/norm(mag_cuboid.magdir);
+magnetdraw(mag_cuboid,[-0.9; 0; 0]);
+
+
+%% Plot setup
+
+figure(2); clf; hold on
+view(3);
+axis equal
+camlight
+
+title('Cylinder magnet')
+xlabel('x');
+ylabel('y');
+zlabel('z');
 
 
 %% Cylinder
@@ -47,20 +59,21 @@ magnetdraw(mag_cuboid,[-0.7; 0; 0]);
 mag_cyl = magnetdefine(...
   'type','cylinder',...
   'dim',[0.075 0.3],...
-  'magdir',[0 0 5],...
+  'magdir',[0 0 0],...
   'magn',1);
 
+mag_cyl.magdir = [0 0 5];
 magnetdraw(mag_cyl,[0.2; 0; 0]);
 
-mag_cuboid.magdir = [5 0 0];
+mag_cyl.magdir = [5 0 0];
 magnetdraw(mag_cyl,[0.4; 0; 0]);
 
-mag_cuboid.magdir = [5 0 5];
+mag_cyl.magdir = [5 0 5];
 magnetdraw(mag_cyl,[0.6; 0; 0]);
 
-mag_cuboid.magdir = [5 0 1];
+mag_cyl.magdir = [5 0 1];
 magnetdraw(mag_cyl,[0.8; 0; 0]);
 
-mag_cuboid.magdir = [5 2 1];
+mag_cyl.magdir = [5 2 1];
 magnetdraw(mag_cyl,[1.0; 0; 0]);
 
--- matlab/magnetdraw.m
+++ matlab/magnetdraw.m
@@ -23,90 +23,83 @@ end
 
 %% Sub-functions
 
-function draw_cube(magnet,pos)
+  function draw_cube(magnet,pos)
+    
+    pos = transpose(pos(:));
+    hdim = magnet.dim/2;
+    
+    vrtc = zeros(8,3);
+    vrtc(1,:) = [-hdim(1); -hdim(2);  hdim(3)]; % top plate
+    vrtc(2,:) = [ hdim(1); -hdim(2);  hdim(3)];
+    vrtc(3,:) = [ hdim(1);  hdim(2);  hdim(3)];
+    vrtc(4,:) = [-hdim(1);  hdim(2);  hdim(3)];
+    vrtc(5,:) = [-hdim(1); -hdim(2); -hdim(3)]; % bottom plate
+    vrtc(6,:) = [ hdim(1); -hdim(2); -hdim(3)];
+    vrtc(7,:) = [ hdim(1);  hdim(2); -hdim(3)];
+    vrtc(8,:) = [-hdim(1);  hdim(2); -hdim(3)];
+    
+    faces(1,:) = [1,2,3,4]; % top
+    faces(2,:) = [5,6,7,8]; % bottom
+    faces(3,:) = [5,6,2,1]; % sides
+    faces(4,:) = [8,7,3,4]; %
+    faces(5,:) = [6,7,3,2]; %
+    faces(6,:) = [5,8,4,1]; %
+    
+    [vrtc_p, faces_p] = split_patches(vrtc,faces,+magnet.magdir);
+    [vrtc_n, faces_n] = split_patches(vrtc,faces,-magnet.magdir);
+    
+    vrtc_p = vrtc_p+pos;
+    vrtc_n = vrtc_n+pos;
+    
+    patch('Faces',faces_p,'Vertices',vrtc_p,'FaceColor',color,'FaceAlpha',1)
+    patch('Faces',faces_n,'Vertices',vrtc_n,'FaceColor',color/2,'FaceAlpha',1)
+    
+    for ii = []%1:size(vrtc_p,1)
+      plot3(vrtc_p(ii,1),vrtc_p(ii,2),vrtc_p(ii,3),'.r','markersize',20)
+      text(vrtc_p(ii,1),vrtc_p(ii,2),vrtc_p(ii,3),num2str(ii),'color','red','fontsize',20)
+    end
+    for ii = []%1:size(vrtc_n,1)
+      plot3(vrtc_n(ii,1),vrtc_n(ii,2),vrtc_n(ii,3),'.b','markersize',20)
+      text(vrtc_n(ii,1),vrtc_n(ii,2),vrtc_n(ii,3),num2str(ii),'color','blue','fontsize',20)
+    end
+    
+  end
 
-pos = transpose(pos(:));
-hdim = magnet.dim/2;
-
-vrtc = zeros(8,3);
-vrtc(1,:) = [-hdim(1); -hdim(2);  hdim(3)]; % top plate
-vrtc(2,:) = [ hdim(1); -hdim(2);  hdim(3)];
-vrtc(3,:) = [ hdim(1);  hdim(2);  hdim(3)];
-vrtc(4,:) = [-hdim(1);  hdim(2);  hdim(3)];
-vrtc(5,:) = [-hdim(1); -hdim(2); -hdim(3)]; % bottom plate
-vrtc(6,:) = [ hdim(1); -hdim(2); -hdim(3)];
-vrtc(7,:) = [ hdim(1);  hdim(2); -hdim(3)];
-vrtc(8,:) = [-hdim(1);  hdim(2); -hdim(3)];
-
-faces(1,:) = [1,2,3,4]; % top
-faces(2,:) = [5,6,7,8]; % bottom
-faces(3,:) = [5,6,2,1]; % sides
-faces(4,:) = [8,7,3,4]; %
-faces(5,:) = [6,7,3,2]; %
-faces(6,:) = [5,8,4,1]; %
-
-[vrtc_p, faces_p] = split_patches(vrtc,faces,+magnet.magdir);
-%[vrtc_n, faces_n] = split_patches(vrtc,faces,-magnet.magdir);
-
-vrtc_p = vrtc_p+pos;
-%vrtc_n = vrtc_n+pos;
-
-NN = size(faces_p,1);
-for ii = 1:NN
-patch('Faces',faces_p(ii,:),'Vertices',vrtc_p,'FaceColor',[ii/NN 0 (NN-ii)/NN],'FaceAlpha',1)
-end
-%patch('Faces',faces_p,'Vertices',vrtc_p,'FaceColor',color,'FaceAlpha',0.5)
-%patch('Faces',faces_n,'Vertices',vrtc_n,'FaceColor',color/2,'FaceAlpha',0.5)
-
-for ii = 1:size(vrtc_p,1)
-  plot3(vrtc_p(ii,1),vrtc_p(ii,2),vrtc_p(ii,3),'.r','markersize',20)
-  text(vrtc_p(ii,1),vrtc_p(ii,2),vrtc_p(ii,3),num2str(ii),'color','red','fontsize',20)
-end
-for ii = []%1:size(vrtc_n,1)
-  plot3(vrtc_n(ii,1),vrtc_n(ii,2),vrtc_n(ii,3),'.b','markersize',20)
-  text(vrtc_n(ii,1),vrtc_n(ii,2),vrtc_n(ii,3),num2str(ii),'color','blue','fontsize',20)
-end
-
-end
-
-function draw_cyl(magnet,pos)
-
-pos = transpose(pos(:));
-
-r = magnet.dim(1);
-h = magnet.dim(2);
-n = 50;
-
-[X,Y,Z] = cylinder(r,n);
-Z(2,:) = h*Z(2,:);
-X = X;
-Y = Y;
-Z = Z-0.5*h;
-
-vrtc = [X(:), Y(:), Z(:)];
-faces = nan(n,4);
-for ii = 1:n
-  faces(ii,:) = 2*(ii-1)+[1 3 4 2];
-end
-
-% Sides
-
-[vrtc_p, faces_p] = split_patches(vrtc,faces,+magnet.magdir);
-[vrtc_n, faces_n] = split_patches(vrtc,faces,-magnet.magdir);
-
-patch('Faces',faces_p,'Vertices',vrtc_p+pos,'FaceColor',color,'EdgeColor','none')
-patch('Faces',faces_n,'Vertices',vrtc_n+pos,'FaceColor',color/2,'EdgeColor','none')
-
-% Bottom & Top cover
-
-faces = [1:2:2*(n+1);2:2:2*(n+1)];
-[vrtc_p, faces_p] = split_patches(vrtc,faces,+magnet.magdir);
-[vrtc_n, faces_n] = split_patches(vrtc,faces,-magnet.magdir);
-
-patch('Faces',faces_p,'Vertices',vrtc_p+pos,'FaceColor',color,'EdgeColor','none')
-patch('Faces',faces_n,'Vertices',vrtc_n+pos,'FaceColor',color/2,'EdgeColor','none')
-
-end
+  function draw_cyl(magnet,pos)
+    
+    pos = transpose(pos(:));
+    
+    r = magnet.dim(1);
+    h = magnet.dim(2);
+    n = 50;
+    
+    [X,Y,Z] = cylinder(r,n);
+    Z = h*Z-0.5*h;
+    
+    vrtc = [X(:), Y(:), Z(:)];
+    faces = nan(n,4);
+    for ii = 1:n
+      faces(ii,:) = 2*(ii-1)+[1 3 4 2];
+    end
+    
+    % Sides
+    
+    [vrtc_p, faces_p] = split_patches(vrtc,faces,+magnet.magdir);
+    [vrtc_n, faces_n] = split_patches(vrtc,faces,-magnet.magdir);
+    
+    patch('Faces',faces_p,'Vertices',vrtc_p+pos,'FaceColor',color,'EdgeColor','none')
+    patch('Faces',faces_n,'Vertices',vrtc_n+pos,'FaceColor',color/2,'EdgeColor','none')
+    
+    % Bottom & Top cover
+    
+    faces = [1:2:2*(n+1);2:2:2*(n+1)];
+    [vrtc_p, faces_p] = split_patches(vrtc,faces,+magnet.magdir);
+    [vrtc_n, faces_n] = split_patches(vrtc,faces,-magnet.magdir);
+    
+    patch('Faces',faces_p,'Vertices',vrtc_p+pos,'FaceColor',color,'EdgeColor','none')
+    patch('Faces',faces_n,'Vertices',vrtc_n+pos,'FaceColor',color/2,'EdgeColor','none')
+    
+  end
 
 end
 
@@ -126,55 +119,76 @@ Nedges = size(faces_new,2)-1;
 for ff = 1:Nfaces
   
   this_face = faces_new(ff,:);
+  this_face(isnan(this_face)) = [];
   new_face = [];
-  vrtc_to_delete = [];
-  faces_update = nan(1,Nfaces-1);
   
-  for vv = 1:Nedges
+  Nfaceedges = numel(this_face)-1;
+  
+  for vv = 1:Nfaceedges
     
-    v1 = vv;
-    v2 = vv+1;
-    faces_update(vv) = faces_new(ff,vv);
+    ind1 = vv;
+    ind2 = vv+1;
+    v1 = this_face(ind1);
+    v2 = this_face(ind2);
     
-    fprintf('Face %i, Edge %i-%i\n',ff,this_face(v1),this_face(v2))
-
-    p1 = vrtc_new(this_face(v1),:);
-    p2 = vrtc_new(this_face(v2),:);
+%    fprintf('Face %i, Edge %i-%i\n',ff,v1,v2)
+    
+    p1 = vrtc_new(v1,:);
+    p2 = vrtc_new(v2,:);
     line_vec = p2-p1;
     
-    pside1 = faces_pn(ff,v1);
-    pside2 = faces_pn(ff,v2);
+    pside1 = faces_pn(ff,ind1);
+    pside2 = faces_pn(ff,ind2);
     
     if pside1 == 0 && pside2 == 0
-      disp('Delete this edge.')
-      vrtc_to_delete = [vrtc_to_delete,faces_new(ff,v1),faces_new(ff,v2)];
+%      disp('Drop this edge.')
     elseif pside1 > 0 && pside2 > 0
-      disp('Keep this edge.')
+%      disp('Keep this edge.')
       new_face = [new_face,v1,v2];
     else
-      disp('Cut this edge.')
+%      disp('Cut this edge.')
       
       s = -dot(normm,p1)/(dot(normm,line_vec));
-      fprintf('s: %2.2f\n',s)
       c = p1+s*line_vec;
-      plot3(c(1),c(2),c(3),'k.','markersize',20)
+      % plot3(c(1),c(2),c(3),'k.','markersize',20)
       
       assert( s>0 && s<=1 , 'Cut not calculated correctly?')
-      vvnew = vv+faces_pn(ff,vv);
-      Nnew = size(vrtc_new,1)+1;
-      vrtc_new(Nnew,:) = c;
-      faces_update(vvnew) = Nnew;
-      fprintf('Edge cut; new vertex %i created.\n',Nnew)
       
+      findc = find(all(abs(vrtc_new-c)<eps,2));
+      if numel(findc) == 0
+        Nnew = size(vrtc_new,1)+1;
+        vrtc_new(Nnew,:) = c;
+%        fprintf('Edge cut; new vertex %i created.\n',Nnew)
+      else
+        if numel(findc) > 1
+          findc = findc(1);
+        end
+        Nnew = findc;
+%        fprintf('Edge cut; vertex %i used.\n',Nnew)
+      end
+      if pside1 > 0
+        new_face = [new_face,v1,Nnew];
+      else
+        new_face = [new_face,Nnew,v2];
+      end
+      
+    end
+    
+    new_face(isnan(new_face)) = [];
+    Nedges_new = numel(new_face)-1;
+    
+    if Nedges_new > Nedges
+      faces_new = [faces_new nan(Nfaces,Nedges_new-Nedges)];
+      Nedges = Nedges_new;
+    elseif Nedges_new < Nedges
+      tmp = nan(1,Nedges+1);
+      tmp(1:(Nedges_new+1)) = new_face;
+      new_face = tmp;
+    end
+    faces_new(ff,:) = new_face;
+    
   end
   
-  
-end
-faces_new(ff,:) = faces_update;
-  for ii = 1:numel(vrtc_to_delete)
-    ind = faces_new(ff,:) == vrtc_to_delete(ii);
-    faces_new(ff,ind) = NaN;
-  end
 end
 
   function faces_pn = calc_face_vertex_side(faces,vrtc)
@@ -191,7 +205,7 @@ end
         faces_pn(fff,vvv) = (1+sign(dot(normm,pp)))/2;
       end
     end
-
+    
   end
 
 end
