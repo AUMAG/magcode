fcca4d3ca1db75a11444bcbe8961726926aca6fc
Will Robertson <wspr81@gmail.com>
2018-12-07 21:44:47 +1030

    update cyl tests

--- matlab/testsuite/testcyl01.m
+++ matlab/testsuite/testcyl01.m
@@ -1,16 +1,21 @@
 
 clear all
 disp('=================')
-fprintf('TEST cylinder forces: ')
+fprintf('TEST cylinder forces match reference: ')
 
-magnet_fixed.dim = [0.02 0.04];
-magnet_float.dim = [0.02 0.04];
-
-magnet_fixed.magn = 1.3;
-magnet_float.magn = 1.3;
+% Equal radii & height
 
 magnet_fixed.type = 'cylinder';
 magnet_float.type = 'cylinder';
+
+magnet_fixed.radius = 0.02;
+magnet_float.radius = 0.02;
+
+magnet_fixed.height = 0.04;
+magnet_float.height = 0.04;
+
+magnet_fixed.magn = 1.3;
+magnet_float.magn = 1.3;
 
 magnet_fixed.dir  = [0; 0; 1];
 magnet_float.dir  = [0; 0; 1]; % must be same
@@ -21,6 +26,16 @@ magnet_float.magdir  = [0; 0; -1];
 F = magnetforces(magnet_fixed,magnet_float,[0; 0; 0.05]);
 
 check = [round(1000*F(3)) 265537];
+assert( check(1) == check(2) , 'Incorrect reference force between cylindrical magnets (calc %i ~= ref %i)',check(1),check(2));
+
+% Different radii & height
+
+magnet_float.radius = 0.015;
+magnet_float.height = 0.03;
+
+F = magnetforces(magnet_fixed,magnet_float,[0; 0; 0.05]);
+
+check = [round(1000*F(3)) 111488];
 assert( check(1) == check(2) , 'Incorrect reference force between cylindrical magnets (calc %i ~= ref %i)',check(1),check(2));
 
 fprintf('passed\n')
--- matlab/testsuite/testcyl02.m
+++ matlab/testsuite/testcyl02.m
@@ -3,24 +3,14 @@ clear all
 disp('=================')
 fprintf('TEST cylinder eccentric force consistency: ')
 
-magnet_fixed.dim = [0.01 0.02];
-magnet_float.dim = [0.01 0.02];
+magnet_fixed = magnetdefine('type','cylinder',...
+  'dim',[0.02 0.02],'magn',1.3,'dir',[0;0;1],'magdir',[0;0;1]);
+magnet_float = magnetdefine('type','cylinder',...
+  'dim',[0.02 0.02],'magn',1.3,'dir',[0;0;1],'magdir',[0;0;1]);
 
-magnet_fixed.magn = 1;
-magnet_float.magn = 1;
-
-magnet_fixed.type = 'cylinder';
-magnet_float.type = 'cylinder';
-
-magnet_fixed.dir  = [0; 0; 1];
-magnet_float.dir  = [0; 0; 1]; % must be same
-
-magnet_fixed.magdir  = [0; 0; +1];
-magnet_float.magdir  = [0; 0; -1];
-
-zdispl = 0.03;
-F1 = magnetforces(magnet_fixed,magnet_float,[0;   0;   zdispl]);
-F2 = magnetforces(magnet_fixed,magnet_float,[eps; eps; zdispl]);
+zdispl = 0.05;
+F1 = magnetforces(magnet_fixed,magnet_float,[    0; 0; zdispl]);
+F2 = magnetforces(magnet_fixed,magnet_float,[ 1e-6; 0; zdispl]);
 
 check = [round(1000*F1(3)) round(1000*F2(3))];
 assert( check(1) == check(2) , 'Inconsistent force with negligible offset (coaxial %i ~= eps offset %i)',check(1),check(2));
