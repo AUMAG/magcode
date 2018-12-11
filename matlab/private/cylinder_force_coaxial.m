b86f8bf64cf129d4b680c3066faa8221bfc46103
Will Robertson <wspr81@gmail.com>
2018-12-11 23:17:22 +1030

    cyl code: avoid indexing, save a line

--- matlab/private/cylinder_force_coaxial.m
+++ matlab/private/cylinder_force_coaxial.m
@@ -20,11 +20,10 @@ K        = zeros(numel(displ),4);
 E        = zeros(numel(displ),4);
 PI_term  = zeros(numel(displ),4);
 
-z = [h1 -h1 h2 -h2]/2;
-ii = [1, 2, 1, 2];
-jj = [3, 3, 4, 4];
+ii = [+1 -1 +1 -1];
+jj = [+1 +1 -1 -1];
 
-a1   = z(ii)-z(jj)-displ;
+a1   = ii.*h1/2-jj.*h2/2-displ;
 a1sq = a1.^2;
 a2   = 1+(r2-r1).^2./a1sq;
 a3sq = (r1+r2).^2 + a1sq ;
@@ -54,7 +53,7 @@ if any(ind(:))
   PI_term(ind) = (1-a1sq(ind)./a3sq(ind)).*PI;
 end
 
-C_d = (-1).^(ii+jj).*a1.*a2.*a3.*(K-E./a2-PI_term);
+C_d = ii.*jj.*a1.*a2.*a3.*(K-E./a2-PI_term);
 f_z = sum(C_d,2);
 
 force_axial = J1*J2/(8*pi*1e-7)*f_z;
