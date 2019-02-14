1ba231b5ebf42614b96fa208b80ab19eee7f2309
Will Robertson <wspr81@gmail.com>
2019-02-14 20:50:04 +1030

    try using function-based tests

--- matlab/runtestsuite.m
+++ matlab/runtestsuite.m
@@ -1,15 +1,23 @@
 %% Wrapper script to run the MAGCODE test suite
-
-addpath(pwd)
-
-choice = 2;
-
-%% Plain execution
 %
 % This code uses the Matlab Unit Test framework.
 % Very little wrapper code is needed to run the test suite, but for
 % Continuous Integration especially it's useful to have the following
 % to ensure that the path is correctly set up.
+
+addpath(pwd)
+
+%% Which code path?
+%
+% By default (for CI) we want to run the profiler and code coverage
+% plug-ins. For development purposes or testing you may wish to switch
+% to |choice=1| to keep things simple and speed things up.
+
+choice = 2;
+
+%% Plain execution
+%
+% Nice and simple.
 
 if choice == 1
   results = runtests('testsuite/');
@@ -37,5 +45,7 @@ if choice == 2
 end
 
 %% Finish up
+%
+% Without this line we don't get the correct exit code if results failed.
 
 assert(all(~[results.Failed]));
--- matlab/testsuite/testcuboidtorque03.m
+++ matlab/testsuite/testcuboidtorque03.m
@@ -1,14 +1,18 @@
-% Test torque zz singularities
+%% Test torque zz singularities
 
-clear all
+function tests = testcuboidtorque03()
+tests = functiontests(localfunctions);
+end
+
+%% Test cuboid torque zz singularity UV
+
+function test_torque_zz_singularity_UV(testCase)
+
 magnet_fixed = magnetdefine('type','cuboid','magn',1.3,'magdir',[0 0 1],'dim',[0.03 0.04 0.05]);
 magnet_float = magnetdefine('type','cuboid','magn',1.3,'magdir',[0 0 1],'dim',[0.04 0.05 0.06]);
 
 smidge = 1e-6;
 prec   = 1e3;
-
-
-%% Test cuboid torque zz singularity UV
 
 displ = [0.005; 0.005; 0.12];
 T1 = magnetforces(magnet_fixed,magnet_float,displ,'torque');
@@ -18,8 +22,17 @@ check = round([T1,T2]*prec);
 assert( all(~isnan(check(:))) , 'UV no nans' )
 assert( all(check(:,1)==check(:,2)) , 'UV singularity consistent' )
 
+end
 
 %% Test cuboid torque zz singularity UW
+
+function test_torque_zz_singularity_UW(testCase)
+
+magnet_fixed = magnetdefine('type','cuboid','magn',1.3,'magdir',[0 0 1],'dim',[0.03 0.04 0.05]);
+magnet_float = magnetdefine('type','cuboid','magn',1.3,'magdir',[0 0 1],'dim',[0.04 0.05 0.06]);
+
+smidge = 1e-6;
+prec   = 1e3;
 
 displ = [0.005; 0.1; 0.005];
 T1 = magnetforces(magnet_fixed,magnet_float,displ,'torque');
@@ -29,8 +42,17 @@ check = round([T1,T2]*prec);
 assert( all(~isnan(check(:))) , 'UW no nans' )
 assert( all(check(:,1)==check(:,2)) , 'UW singularity consistent' )
 
+end
 
 %% Test cuboid torque zz singularity WV
+
+function test_torque_zz_singularity_WV(testCase)
+
+magnet_fixed = magnetdefine('type','cuboid','magn',1.3,'magdir',[0 0 1],'dim',[0.03 0.04 0.05]);
+magnet_float = magnetdefine('type','cuboid','magn',1.3,'magdir',[0 0 1],'dim',[0.04 0.05 0.06]);
+
+smidge = 1e-6;
+prec   = 1e3;
 
 displ = [0.08; 0.005; 0.005];
 T1 = magnetforces(magnet_fixed,magnet_float,displ,'torque');
@@ -40,3 +62,4 @@ check = round([T1,T2]*prec);
 assert( all(~isnan(check(:))) , 'UW no nans' )
 assert( all(check(:,1)==check(:,2)) , 'UW singularity consistent' )
 
+end
