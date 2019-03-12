c53e20e02c235b87ca27729c6b16a09fdcca1ec8
Will Robertson <wspr81@gmail.com>
2019-03-13 09:38:28 +1030

    dipole comparison still eluding me

--- matlab/examples/dipole_comparison.m
+++ matlab/examples/dipole_comparison.m
@@ -1,13 +1,25 @@
 %% Dipole comparison example
 
+%% verify
 
-%% Parallel magnets example
+ma = [0;0;100];
+mb = [0;0;100];
+rab = [0;0;1];
+calcdipoleforce(rab,ma,mb)
 
-coil = magnetdefine('type','cylinder','turns',100,'current',1,'dim',[0.02 0.04]);
-mag  = magnetdefine('type','cylinder','dim',[0.005 0.005],'magn',1);
+ma = [0;-100;0];
+mb = [0;0;100];
+rab = [0;0;1];
+calcdipoleforce(rab,ma,mb)
+
+%%
+
+a = 0.02;
+coil = magnetdefine('type','cuboid','magn',1,'magdir',[0;0;1],'dim',[a a a]);
+mag  = magnetdefine('type','cuboid','magn',1,'magdir',[0;0;1],'dim',[a a a]);
 
 N = 50;
-displ = [0; 0; 1]*linspace(0.03,0.1,N);
+displ = [0; 0; 1]*linspace(5*a,20*a,N);
 
 coilmagforce = magnetforces(coil,mag,displ);
 
@@ -19,9 +31,10 @@ end
 figure(1); clf; hold on
 plot(displ(3,:),coilmagforce(3,:))
 plot(displ(3,:),dipoleforce(3,:))
+legend('Exact','Dipole')
 xlabel('Axial displacement, m')
 ylabel('Axial force, N')
-set(gca,'box','on','ticklength',[0.02 0.05])
+set(gca,'box','on','ticklength',[0.02 0.05],'xscale','log','yscale','log')
 
 function f = calcdipoleforce(r_ab,m_a,m_b)
 
