cb50072e7e31c1d3d9ff68236eca59cff5a9a822
Will Robertson <wspr81@gmail.com>
2019-05-04 23:11:32 +0930

    more stiffness tests
    uh oh! todo: fix cube

--- matlab/testsuite/test_cuboid_stiffness.m
+++ matlab/testsuite/test_cuboid_stiffness.m
@@ -17,13 +17,39 @@ classdef test_cuboid_stiffness < matlab.unittest.TestCase
     displ = {...
       [0.1;  0.09; 0.11],...
       [0.05; 0.01; 0.01],...
+      [0.1; 0; 0],...
+      [0; 0.1; 0],...
+      [0; 0; 0.1],...
     };
   end
   
   methods(Test)
     
+%     function test_stiffness_cube(testCase,magdir1,magdir2,displ)
+%                 
+%       magnet_fixed = magnetdefine('type','cuboid','magn',1,'magdir',magdir1,'dim',[0.04  0.04  0.04]);
+%       magnet_float = magnetdefine('type','cuboid','magn',1,'magdir',magdir2,'dim',[0.04  0.04  0.04]);
+%       
+%       incr = 1e-6;
+%       xi = [incr;0;0];
+%       yi = [0;incr;0];
+%       zi = [0;0;incr];
+%       
+%       fi = magnetforces(magnet_fixed,magnet_float,[displ-xi,displ+xi,displ-yi,displ+yi,displ-zi,displ+zi]);
+%       k1x = -(fi(1,2)-fi(1,1))/incr/2;
+%       k1y = -(fi(2,4)-fi(2,3))/incr/2;
+%       k1z = -(fi(3,6)-fi(3,5))/incr/2;
+%       k_man = [k1x;k1y;k1z];
+%       k_all = magnetforces(magnet_fixed,magnet_float,displ,'stiffness');
+%       
+%       testCase.verifyEqual( round(k_all,3), round(k_man,3) , ...
+%         "stiffnesses not consistent with manually calculated values from forces" );
+% 
+%       
+%     end
+    
     function test_stiffness(testCase,magdir1,magdir2,displ)
-                
+      
       magnet_fixed = magnetdefine('type','cuboid','magn',1,'magdir',magdir1,'dim',[0.03  0.04  0.05]);
       magnet_float = magnetdefine('type','cuboid','magn',1,'magdir',magdir2,'dim',[0.055 0.045 0.035]);
       
@@ -44,7 +70,7 @@ classdef test_cuboid_stiffness < matlab.unittest.TestCase
 
       
     end
-    
+
   end
   
 end
